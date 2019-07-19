9eccac671928e5ee24b1899c47f9521a46b24bf0
Maksym Zavershynskyi <max@near.ai>
2019-07-19 15:53:13 -0700

    Move mission control to examples folder

--- examples/mission-control/build.sh
+++ examples/mission-control/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 wasm-pack build --no-typescript --release
-wasm-opt -Oz --output ./pkg/optimized_contract.wasm ./pkg/test_contract_bg.wasm
+wasm-opt -Oz --output ./pkg/optimized_contract.wasm ./pkg/mission_control_bg.wasm
 cp pkg/optimized_contract.wasm ./res/mission_control.wasm
 rm -rf pkg
