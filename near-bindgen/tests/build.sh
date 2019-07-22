4ae975427dfd528073c315cf58f93093b45cf776
Maksym Zavershynskyi <max@near.ai>
2019-07-22 15:20:10 -0700

    Remove integration tests with nearcore to make it publishable

--- near-bindgen/tests/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pushd $(dirname "$0")/test-contract
-wasm-pack build --no-typescript --release
-wasm-opt -Oz --output ./pkg/optimized_contract.wasm ./pkg/test_contract_bg.wasm
-cp pkg/optimized_contract.wasm ../res/test_contract.wasm
-rm -rf pkg
-popd
--- near-bindgen/tests/test-contract/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![feature(const_vec_new)]
-
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-pub mod simple_contract;
