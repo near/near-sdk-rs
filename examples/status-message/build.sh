fda9aeb51de44aa19a194d34c5d6f5d8c38f1717
Maksym Zavershynskyi <max@near.ai>
2019-09-07 14:33:03 -0700

    Update instructions on wasm-gc

--- examples/status-message/build.sh
+++ examples/status-message/build.sh
@@ -3,6 +3,6 @@ set -e
 
 cargo +nightly build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/status_message.wasm ./res/
-wasm-opt -Oz --output ./res/status_message.wasm ./res/status_message.wasm
+#wasm-opt -Oz --output ./res/status_message.wasm ./res/status_message.wasm
 wasm-gc ./res/status_message.wasm
 rm -rf target
