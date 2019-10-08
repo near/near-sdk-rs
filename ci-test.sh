005243c190734be6bae9bbcf1bcab26dcc260638
Bo Yao <bo@nearprotocol.com>
2019-10-08 14:26:32 -0700

    use magic comment, fix typo

--- ci-test.sh
+++ ci-test.sh
@@ -10,12 +10,8 @@ else
 
     cp near-bindgen/Cargo.toml{,.bak}
     cp Cargo.lock{,.bak}
-    borsh_git='borsh = { git = "https://github.com/nearprotocol/borsh", branch = "master" }'
-    vm_logic_git='near-vm-logic = { git = "https://github.com/nearprotocol/nearcore", branch = "staging" }'
-    sed -i "s|^borsh.*|${borsh_git}|" near-bindgen/Cargo.toml
-    sed -i "s|^near-vm-logic.*|${vm_logic_git}|" near-bindgen/Cargo.toml
-    sed -n '/^borsh/p' near-bindgen/Cargo.toml 
-    sed -n '/^near-vm-logic/p' near-bindgen/Cargo.toml
+
+    sed -i "s|###||g" near-bindgen/Cargo.toml
     
     set +e
     cargo test --all
