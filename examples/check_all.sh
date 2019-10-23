dc6415052a01c17765b31bd4b1fd16b111c73506
Evgeny Kuzyakov <ek@nearprotocol.com>
2019-10-23 15:41:52 -0700

    Try delete rust-toolchain to support nightly tests

--- examples/check_all.sh
+++ examples/check_all.sh
@@ -6,7 +6,7 @@ pushd $(dirname $0)
 for d in */ ; do
     pushd $d
     echo "Checking $d compiles for wasm32 target"
-    RUSTFLAGS='-C link-arg=-s' cargo +nightly check --target wasm32-unknown-unknown --release
+    RUSTFLAGS='-C link-arg=-s' cargo check --target wasm32-unknown-unknown --release
     popd
 done
 
