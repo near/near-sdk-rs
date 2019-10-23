2defb28ca60a1c7ac9b3c35e4c1cc6ab3535fc34
Evgeny Kuzyakov <ek@nearprotocol.com>
2019-10-23 14:10:58 -0700

    Triggering CI

--- examples/check_all.sh
+++ examples/check_all.sh
@@ -5,7 +5,7 @@ pushd $(dirname $0)
 
 for d in */ ; do
     pushd $d
-    echo "Checking $d compiles"
+    echo "Checking $d compiles for wasm32 target"
     RUSTFLAGS='-C link-arg=-s' cargo +nightly check --target wasm32-unknown-unknown --release
     popd
 done
