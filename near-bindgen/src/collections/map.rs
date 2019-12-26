cdae46af46c2b3c88e063d602f87317a4daad771
Maksym Zavershynskyi <35039879+nearmax@users.noreply.github.com>
2019-12-26 10:30:05 -0800

    Add more tests for collections (#48)

--- near-bindgen/src/collections/map.rs
+++ near-bindgen/src/collections/map.rs
@@ -216,3 +216,229 @@ impl Iterator for IntoMapRawValues {
         }
     }
 }
+
+#[cfg(not(target_arch = "wasm32"))]
+#[cfg(test)]
+mod tests {
+    use near_vm_logic::types::AccountId;
+    use near_vm_logic::VMContext;
+    use crate::{env, MockedBlockchain};
+    use crate::collections::Map;
+    use rand::{SeedableRng, Rng};
+    use rand::seq::SliceRandom;
+    use std::collections::{HashMap, HashSet};
+    use std::iter::FromIterator;
+
+    fn alice() -> AccountId {
+        "alice.near".to_string()
+    }
+    fn bob() -> AccountId {
+        "bob.near".to_string()
+    }
+    fn carol() -> AccountId {
+        "carol.near".to_string()
+    }
+
+    fn set_env() {
+        let context = VMContext {
+            current_account_id: alice(),
+            signer_account_id: bob(),
+            signer_account_pk: vec![0, 1, 2],
+            predecessor_account_id: carol(),
+            input: vec![],
+            block_index: 0,
+            block_timestamp: 0,
+            account_balance: 0,
+            account_locked_balance: 0,
+            storage_usage: 10u64.pow(6),
+            attached_deposit: 0,
+            prepaid_gas: 10u64.pow(9),
+            random_seed: vec![0, 1, 2],
+            is_view: false,
+            output_data_receivers: vec![],
+        };
+        let storage = match env::take_blockchain_interface() {
+            Some(mut bi) => bi.as_mut_mocked_blockchain().unwrap().take_storage(),
+            None => Default::default(),
+        };
+        env::set_blockchain_interface(Box::new(MockedBlockchain::new(
+        context,
+        Default::default(),
+        Default::default(),
+        vec![],
+        storage,
+        )));
+    }
+
+    #[test]
+    pub fn test_insert() {
+        set_env();
+        let mut map = Map::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(0);
+        for _ in 0..10_000 {
+            let key = rng.gen::<u64>();
+            let value = rng.gen::<u64>();
+            map.insert(&key, &value);
+        }
+    }
+
+    #[test]
+    pub fn test_insert_remove() {
+        set_env();
+        let mut map = Map::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(1);
+        let mut keys = vec![];
+        let mut key_to_value = HashMap::new();
+        for _ in 0..10_000 {
+            let key = rng.gen::<u64>();
+            let value = rng.gen::<u64>();
+            keys.push(key);
+            key_to_value.insert(key, value);
+            map.insert(&key, &value);
+        }
+        keys.shuffle(&mut rng);
+        for key in keys {
+            let actual = map.remove(&key).unwrap();
+            assert_eq!(actual, key_to_value[&key]);
+        }
+    }
+
+    #[test]
+    pub fn test_insert_override_remove() {
+        set_env();
+        let mut map = Map::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(2);
+        let mut keys = vec![];
+        let mut key_to_value = HashMap::new();
+        for _ in 0..10_000 {
+            let key = rng.gen::<u64>();
+            let value = rng.gen::<u64>();
+            keys.push(key);
+            key_to_value.insert(key, value);
+            map.insert(&key, &value);
+        }
+        keys.shuffle(&mut rng);
+        for key in &keys {
+            let value = rng.gen::<u64>();
+            let actual = map.insert(key, &value).unwrap();
+            assert_eq!(actual, key_to_value[key]);
+            key_to_value.insert(*key, value);
+        }
+        keys.shuffle(&mut rng);
+        for key in keys {
+            let actual = map.remove(&key).unwrap();
+            assert_eq!(actual, key_to_value[&key]);
+        }
+    }
+
+    #[test]
+    pub fn test_get_non_existent() {
+        set_env();
+        let mut map = Map::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(3);
+        let mut key_to_value = HashMap::new();
+        for _ in 0..10_000 {
+            let key = rng.gen::<u64>() % 20_000;
+            let value = rng.gen::<u64>();
+            key_to_value.insert(key, value);
+            map.insert(&key, &value);
+        }
+        for _ in 0..10_000 {
+            let key = rng.gen::<u64>() % 20_000;
+            assert_eq!(map.get(&key), key_to_value.get(&key).cloned());
+        }
+    }
+
+    #[test]
+    pub fn test_to_vec() {
+        set_env();
+        let mut map = Map::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(4);
+        let mut key_to_value = HashMap::new();
+        for _ in 0..10_000 {
+            let key = rng.gen::<u64>();
+            let value = rng.gen::<u64>();
+            key_to_value.insert(key, value);
+            map.insert(&key, &value);
+        }
+        let actual = HashMap::from_iter(map.to_vec());
+        assert_eq!(actual, key_to_value);
+    }
+
+    #[test]
+    pub fn test_clear() {
+        set_env();
+        let mut map = Map::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(5);
+        for _ in 0..100 {
+            for _ in 0..=(rng.gen::<u64>() % 200 + 1) {
+                let key = rng.gen::<u64>();
+                let value = rng.gen::<u64>();
+                map.insert(&key, &value);
+            }
+            assert!(!map.to_vec().is_empty());
+            map.clear();
+            assert!(map.to_vec().is_empty());
+        }
+    }
+
+    #[test]
+    pub fn test_keys_values() {
+        set_env();
+        let mut map = Map::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(4);
+        let mut key_to_value = HashMap::new();
+        for _ in 0..10_000 {
+            let key = rng.gen::<u64>();
+            let value = rng.gen::<u64>();
+            key_to_value.insert(key, value);
+            map.insert(&key, &value);
+        }
+        let actual: HashMap<u64, u64> = HashMap::from_iter(map.to_vec());
+        assert_eq!(actual.keys().collect::<HashSet<_>>(), key_to_value.keys().collect::<HashSet<_>>());
+        assert_eq!(actual.values().collect::<HashSet<_>>(), key_to_value.values().collect::<HashSet<_>>());
+    }
+
+    #[test]
+    pub fn test_iter() {
+        set_env();
+        let mut map = Map::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(4);
+        let mut key_to_value = HashMap::new();
+        for _ in 0..10_000 {
+            let key = rng.gen::<u64>();
+            let value = rng.gen::<u64>();
+            key_to_value.insert(key, value);
+            map.insert(&key, &value);
+        }
+        let actual: HashMap<u64, u64> = HashMap::from_iter(map.iter());
+        assert_eq!(actual, key_to_value);
+    }
+
+    #[test]
+    pub fn test_extend() {
+        set_env();
+        let mut map = Map::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(4);
+        let mut key_to_value = HashMap::new();
+        for _ in 0..100 {
+            let key = rng.gen::<u64>();
+            let value = rng.gen::<u64>();
+            key_to_value.insert(key, value);
+            map.insert(&key, &value);
+        }
+        for _ in 0..100 {
+            let mut tmp = vec![];
+            for _ in 0..=(rng.gen::<u64>() % 200 + 1) {
+                let key = rng.gen::<u64>();
+                let value = rng.gen::<u64>();
+                tmp.push((key, value));
+            }
+            key_to_value.extend(tmp.iter().cloned());
+            map.extend(tmp.iter().cloned());
+        }
+
+        let actual: HashMap<u64, u64> = HashMap::from_iter(map.iter());
+        assert_eq!(actual, key_to_value);
+    }
+}
--- near-bindgen/src/collections/set.rs
+++ near-bindgen/src/collections/set.rs
@@ -131,3 +131,194 @@ impl Iterator for IntoSetRawElements {
         }
     }
 }
+
+#[cfg(not(target_arch = "wasm32"))]
+#[cfg(test)]
+mod tests {
+    use near_vm_logic::types::AccountId;
+    use near_vm_logic::VMContext;
+    use crate::{env, MockedBlockchain};
+    use crate::collections::Set;
+    use rand::{SeedableRng, Rng};
+    use rand::seq::SliceRandom;
+    use std::collections::HashSet;
+    use std::iter::FromIterator;
+
+    fn alice() -> AccountId {
+        "alice.near".to_string()
+    }
+    fn bob() -> AccountId {
+        "bob.near".to_string()
+    }
+    fn carol() -> AccountId {
+        "carol.near".to_string()
+    }
+
+    fn set_env() {
+        let context = VMContext {
+            current_account_id: alice(),
+            signer_account_id: bob(),
+            signer_account_pk: vec![0, 1, 2],
+            predecessor_account_id: carol(),
+            input: vec![],
+            block_index: 0,
+            block_timestamp: 0,
+            account_balance: 0,
+            account_locked_balance: 0,
+            storage_usage: 10u64.pow(6),
+            attached_deposit: 0,
+            prepaid_gas: 10u64.pow(9),
+            random_seed: vec![0, 1, 2],
+            is_view: false,
+            output_data_receivers: vec![],
+        };
+        let storage = match env::take_blockchain_interface() {
+            Some(mut bi) => bi.as_mut_mocked_blockchain().unwrap().take_storage(),
+            None => Default::default(),
+        };
+        env::set_blockchain_interface(Box::new(MockedBlockchain::new(
+            context,
+            Default::default(),
+            Default::default(),
+            vec![],
+            storage,
+        )));
+    }
+
+    #[test]
+    pub fn test_insert() {
+        set_env();
+        let mut set = Set::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(0);
+        for _ in 0..10_000 {
+            let key = rng.gen::<u64>();
+            set.insert(&key);
+        }
+    }
+
+    #[test]
+    pub fn test_insert_remove() {
+        set_env();
+        let mut set = Set::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(1);
+        let mut keys = vec![];
+        for _ in 0..10_000 {
+            let key = rng.gen::<u64>();
+            keys.push(key);
+            set.insert(&key);
+        }
+        keys.shuffle(&mut rng);
+        for key in keys {
+            assert!(set.remove(&key));
+        }
+    }
+
+    #[test]
+    pub fn test_insert_override_remove() {
+        set_env();
+        let mut set = Set::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(2);
+        let mut keys = vec![];
+        for _ in 0..10_000 {
+            let key = rng.gen::<u64>();
+            keys.push(key);
+            set.insert(&key);
+        }
+        keys.shuffle(&mut rng);
+        for key in &keys {
+            assert!(set.insert(key));
+        }
+        keys.shuffle(&mut rng);
+        for key in keys {
+            assert!(set.remove(&key));
+        }
+    }
+
+    #[test]
+    pub fn test_contains_non_existent() {
+        set_env();
+        let mut set = Set::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(3);
+        let mut set_tmp = HashSet::new();
+        for _ in 0..10_000 {
+            let key = rng.gen::<u64>() % 20_000;
+            set_tmp.insert(key);
+            set.insert(&key);
+        }
+        for _ in 0..10_000 {
+            let key = rng.gen::<u64>() % 20_000;
+            assert_eq!(set.contains(&key), set_tmp.contains(&key));
+        }
+    }
+
+    #[test]
+    pub fn test_to_vec() {
+        set_env();
+        let mut set = Set::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(4);
+        let mut keys = HashSet::new();
+        for _ in 0..10_000 {
+            let key = rng.gen::<u64>();
+            keys.insert(key);
+            set.insert(&key);
+        }
+        let actual = HashSet::from_iter(set.to_vec());
+        assert_eq!(actual, keys);
+    }
+
+    #[test]
+    pub fn test_clear() {
+        set_env();
+        let mut set = Set::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(5);
+        for _ in 0..100 {
+            for _ in 0..=(rng.gen::<u64>() % 200 + 1) {
+                let key = rng.gen::<u64>();
+                set.insert(&key);
+            }
+            assert!(!set.to_vec().is_empty());
+            set.clear();
+            assert!(set.to_vec().is_empty());
+        }
+    }
+
+    #[test]
+    pub fn test_iter() {
+        set_env();
+        let mut set = Set::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(4);
+        let mut keys = HashSet::new();
+        for _ in 0..10_000 {
+            let key = rng.gen::<u64>();
+            keys.insert(key);
+            set.insert(&key);
+        }
+        let actual: HashSet<u64> = HashSet::from_iter(set.iter());
+        assert_eq!(actual, keys);
+    }
+
+    #[test]
+    pub fn test_extend() {
+        set_env();
+        let mut set = Set::default();
+        let mut rng = rand_xorshift::XorShiftRng::seed_from_u64(4);
+        let mut keys = HashSet::new();
+        for _ in 0..100 {
+            let key = rng.gen::<u64>();
+            keys.insert(key);
+            set.insert(&key);
+        }
+        for _ in 0..100 {
+            let mut tmp = vec![];
+            for _ in 0..=(rng.gen::<u64>() % 200 + 1) {
+                let key = rng.gen::<u64>();
+                tmp.push(key);
+            }
+            keys.extend(tmp.iter().cloned());
+            set.extend(tmp.iter().cloned());
+        }
+
+        let actual: HashSet<u64> = HashSet::from_iter(set.iter());
+        assert_eq!(actual, keys);
+    }
+}
