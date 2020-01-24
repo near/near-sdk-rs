cd6774a559e4a6f5f76cbf5e9393e34ebee2c390
Jane <jane.degtiareva@gmail.com>
2020-01-23 16:58:20 -0800

    Keep target dir in build.sh of examples to improve developer experience
    This speeds up builds significantly

--- examples/mission-control/build.sh
+++ examples/mission-control/build.sh
@@ -4,4 +4,4 @@ set -e
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/mission_control.wasm ./res/
 #wasm-opt -Oz --output ./res/mission_control.wasm ./res/mission_control.wasm
-rm -rf target
+
--- examples/status-message-collections/build.sh
+++ examples/status-message-collections/build.sh
@@ -4,4 +4,4 @@ set -e
 RUSTFLAGS='-C link-arg=-s' cargo build --target wasm32-unknown-unknown --release
 cp target/wasm32-unknown-unknown/release/status_message_collections.wasm ./res/
 #wasm-opt -Oz --output ./res/status_message_collections.wasm ./res/status_message_collections.wasm
-rm -rf target
+
