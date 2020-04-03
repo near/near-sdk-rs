7c97fdbc6ab025c10c27b58c25259f2e5cc3fd5a
Willem Wyndham <willem@cs.umd.edu>
2020-04-03 12:45:12 -0400

    remove binaryen from test

--- near-mock-vm/tests/index.js
+++ near-mock-vm/tests/index.js
@@ -3,24 +3,20 @@ v8.setFlagsFromString('--experimental-wasm-bigint');
 
 let VM = require('../dist').NearVM;
 let fs = require("fs");
-const binaryen = require("binaryen");
+// const binaryen = require("binaryen");
 
 
-// Proxy Binaryen's ready event
-// Object.defineProperty(b, "ready", {
-//   get: function() { return binaryen.ready; }
-// });
-binaryen.ready.then((b) => {
-  let bin = fs.readFileSync(__dirname + "/../out/main.wasm")
-  let instd = Buffer.from(VM.instrumentBinary(bin));
-  const mod = b.readBinary(instd);
-  mod.validate();
-  debugger;
-  b.setDebugInfo(true);
-  VM.run(bin, "add", JSON.stringify({"a": 1000, "b": 1000}))
+
+// binaryen.ready.then((b) => {
+  let bin = fs.readFileSync(__dirname + "/../out/main.wasm");
+  // let instd = Buffer.from(VM.instrumentBinary(bin));
+  // const mod = b.readBinary(instd);
+  // mod.validate();
+
+  VM.run(bin, "add", JSON.stringify({"a": 1000, "b": 1000}));
   // mod.setDebugInfo(true);
 
   // let mod = new WebAssembly.Module(instd);
 
-});
+// });
 // let bin = fs.readFileSync(__dirname + "/");
