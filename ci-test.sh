ab9a0deb0a1274a89161f75e2c3655000d623545
Bo Yao <bo@nearprotocol.com>
2020-07-14 17:10:47 -0700

    Size all example contracts in CI

--- ci-test.sh
+++ ci-test.sh
@@ -25,8 +25,10 @@ else
     fi
 
     # Only testing it for one configuration to avoid running the same tests twice
-    echo "Checking compilation of wasm32 for all examples"
-    ./examples/check_all.sh
+    echo "Build wasm32 for all examples"
+    ./examples/build_all.sh
+    echo "Checking size of all example contracts"
+    ./examples/size_all.sh
     echo "Testing all examples"
     ./examples/test_all.sh
 fi
