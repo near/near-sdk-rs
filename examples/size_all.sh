5ddbcca853718a0916bff7f481481d11c9754e74
Bo Yao <bo@nearprotocol.com>
2020-07-14 17:11:37 -0700

    add size_all

---
+++ examples/size_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+rm -rf wasm_sizer
+git clone https://github.com/near/wasm_sizer
+pip3 install --user numpy matplotlib
+
+for contract in $(ls examples/*/res/*.wasm); do
+    echo "Size contract $contract"
+    python3 ../wasm_sizer/sizer.py --input $contract --sections --silent
+done
