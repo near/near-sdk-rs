d3199c0d159a87ad846647cfb2b92eb5ca5e3738
Nikolay Igotti <igotti@gmail.com>
2020-07-17 17:30:33 +0300

    Add examples minifier.

---
+++ minifier/minify_examples.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+pushd ../examples
+KEEP_NAMES=1 ./build_all.sh
+popd
+mkdir -p ./out/base/
+for d in ../examples/*/ ; do
+  cp $d/res/*.wasm ./out/base/
+done
+
+#for p in /work/near/core-contracts/*/res/*.wasm ; do
+#  cp $p ./out/base/
+#done
+
+cd out
+for p in ./base/*.wasm ; do
+  w=$(basename -- $p)
+  wasm-snip $p --snip-rust-fmt-code --snip-rust-panicking-code -p core::num::flt2dec::.* -p core::fmt::float::.*  \
+     --output snipped-$w
+  wasm-gc snipped-$w
+  wasm-strip snipped-$w
+  cp $p stripped-$w
+  wasm-strip stripped-$w
+  echo $w `stat -c "%s" stripped-$w` " -> " `stat -c "%s" snipped-$w`
+done
