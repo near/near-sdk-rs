44f55b057684dacf79e6195d90b1dd877ec1242b
Evgeny Kuzyakov <ek@nearprotocol.com>
2021-03-05 16:21:22 -0800

    Replace Base64 into InlineSVG

--- examples/fungible-token/ft/src/lib.rs
+++ examples/fungible-token/ft/src/lib.rs
@@ -33,7 +33,7 @@ pub struct Contract {
     metadata: LazyOption<FungibleTokenMetadata>,
 }
 
-const BASE_64_NEAR_ICON: &str = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyODggMjg4Ij48ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIj48cGF0aCBkPSJNMTg3LjU4LDc5LjgxbC0zMC4xLDQ0LjY5YTMuMiwzLjIsMCwwLDAsNC43NSw0LjJMMTkxLjg2LDEwM2ExLjIsMS4yLDAsMCwxLDIsLjkxdjgwLjQ2YTEuMiwxLjIsMCwwLDEtMi4xMi43N0wxMDIuMTgsNzcuOTNBMTUuMzUsMTUuMzUsMCwwLDAsOTAuNDcsNzIuNUg4Ny4zNEExNS4zNCwxNS4zNCwwLDAsMCw3Miw4Ny44NFYyMDEuMTZBMTUuMzQsMTUuMzQsMCwwLDAsODcuMzQsMjE2LjVoMGExNS4zNSwxNS4zNSwwLDAsMCwxMy4wOC03LjMxbDMwLjEtNDQuNjlhMy4yLDMuMiwwLDAsMC00Ljc1LTQuMkw5Ni4xNCwxODZhMS4yLDEuMiwwLDAsMS0yLS45MVYxMDQuNjFhMS4yLDEuMiwwLDAsMSwyLjEyLS43N2w4OS41NSwxMDcuMjNhMTUuMzUsMTUuMzUsMCwwLDAsMTEuNzEsNS40M2gzLjEzQTE1LjM0LDE1LjM0LDAsMCwwLDIxNiwyMDEuMTZWODcuODRBMTUuMzQsMTUuMzQsMCwwLDAsMjAwLjY2LDcyLjVoMEExNS4zNSwxNS4zNSwwLDAsMCwxODcuNTgsNzkuODFaIi8+PC9nPjwvc3ZnPg==";
+const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
 
 #[near_bindgen]
 impl Contract {
@@ -48,7 +48,7 @@ impl Contract {
                 spec: FT_METADATA_SPEC.to_string(),
                 name: "Example NEAR fungible token".to_string(),
                 symbol: "EXAMPLE".to_string(),
-                icon: Some(BASE_64_NEAR_ICON.to_string()),
+                icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
                 reference: None,
                 reference_hash: None,
                 decimals: 24,
