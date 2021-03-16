5beea667b8ccf17e861d855e9fedd89924a811db
Robert Zaremba <robert@zaremba.ch>
2021-03-16 18:03:02 +0100

    contract standards: add ext_fungible_token (#340)
    When implementing contracts which operate with fungible tokens, it's useful to use `#[ext_contract(ext_fungible_token)]` macro.
    
    This PR adds a canonical implementation of that wrapper to near-contract-standards.
    
    * contract standards: add ext_fungible_token
    
    * Apply suggestions from code review
    
    Co-authored-by: Evgeny Kuzyakov <h3r0k1ll3r@gmail.com>
    
    Co-authored-by: Evgeny Kuzyakov <h3r0k1ll3r@gmail.com>
    Co-authored-by: Evgeny Kuzyakov <ek@nearprotocol.com>

--- near-contract-standards/src/fungible_token/core_impl.rs
+++ near-contract-standards/src/fungible_token/core_impl.rs
@@ -33,6 +33,25 @@ pub trait FungibleTokenReceiver {
     ) -> PromiseOrValue<U128>;
 }
 
+#[ext_contract(ext_fungible_token)]
+pub trait FungibleTokenContract {
+    fn ft_transfer(&mut self, receiver_id: AccountId, amount: U128, memo: Option<String>);
+
+    fn ft_transfer_call(
+        &mut self,
+        receiver_id: AccountId,
+        amount: U128,
+        memo: Option<String>,
+        msg: String,
+    ) -> PromiseOrValue<U128>;
+
+    /// Returns the total supply of the token in a decimal string representation.
+    fn ft_total_supply(&self) -> U128;
+
+    /// Returns the balance of the account. If the account doesn't exist must returns `"0"`.
+    fn ft_balance_of(&self, account_id: AccountId) -> U128;
+}
+
 /// Implementation of a FungibleToken standard.
 /// Allows to include NEP-141 compatible token to any contract.
 /// There are next traits that any contract may implement:
