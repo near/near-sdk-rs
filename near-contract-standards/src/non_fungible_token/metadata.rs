c3284423effe89832ac0b57bddad98a1240c87e5
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-04-08 20:35:13 -0400

    update metadata

--- near-contract-standards/src/non_fungible_token/metadata.rs
+++ near-contract-standards/src/non_fungible_token/metadata.rs
@@ -1,31 +1,41 @@
-use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::json_types::Base64VecU8;
-use near_sdk::serde::{Deserialize, Serialize};
+use crate::*;
 
-pub const NFT_METADATA_SPEC: &str = "nft-1.0.0";
-
-#[derive(BorshDeserialize, BorshSerialize, Clone, Deserialize, Serialize)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
 #[serde(crate = "near_sdk::serde")]
-pub struct NonFungibleTokenMetadata {
-    pub spec: String,
-    pub name: String,
-    pub symbol: String,
-    pub icon: Option<String>,
-    pub reference: Option<String>,
-    pub reference_hash: Option<Base64VecU8>,
-    pub decimals: u8,
+pub struct NFTContractMetadata {
+    pub spec: String,              // required, essentially a version like "nft-1.0.0"
+    pub name: String,              // required, ex. "Mosaics"
+    pub symbol: String,            // required, ex. "MOSIAC"
+    pub icon: Option<String>,      // Data URL
+    pub base_uri: Option<String>, // Centralized gateway known to have reliable access to decentralized storage assets referenced by `reference` or `media` URLs
+    pub reference: Option<String>, // URL to a JSON file with more info
+    pub reference_hash: Option<Base64VecU8>, // Base64-encoded sha256 hash of JSON from reference field. Required if `reference` is included.
 }
 
-pub trait NonFungibleTokenMetadataProvider {
-    fn nft_metadata(&self) -> NonFungibleTokenMetadata;
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct TokenMetadata {
+    pub title: Option<String>, // ex. "Arch Nemesis: Mail Carrier" or "Parcel #5055"
+    pub description: Option<String>, // free-form description
+    pub media: Option<String>, // URL to associated media, preferably to decentralized, content-addressed storage
+    pub media_hash: Option<Base64VecU8>, // Base64-encoded sha256 hash of content referenced by the `media` field. Required if `media` is included.
+    pub copies: Option<U64>, // number of copies of this set of metadata in existence when token was minted.
+    pub issued_at: Option<String>, // ISO 8601 datetime when token was issued or minted
+    pub expires_at: Option<String>, // ISO 8601 datetime when token expires
+    pub starts_at: Option<String>, // ISO 8601 datetime when token starts being valid
+    pub updated_at: Option<String>, // ISO 8601 datetime when token was last updated
+    pub extra: Option<String>, // anything extra the NFT wants to store on-chain. Can be stringified JSON.
+    pub reference: Option<String>, // URL to an off-chain JSON file with more info.
+    pub reference_hash: Option<Base64VecU8>, // Base64-encoded sha256 hash of JSON from reference field. Required if `reference` is included.
 }
 
-impl NonFungibleTokenMetadata {
-    pub fn assert_valid(&self) {
-        assert_eq!(&self.spec, NFT_METADATA_SPEC);
-        assert_eq!(self.reference.is_some(), self.reference_hash.is_some());
-        if let Some(reference_hash) = &self.reference_hash {
-            assert_eq!(reference_hash.0.len(), 32, "Hash has to be 32 bytes");
-        }
+pub trait NonFungibleTokenMetadata {
+    fn nft_metadata(&self) -> NFTMetadata;
+}
+
+#[near_bindgen]
+impl NonFungibleTokenMetadata for Contract {
+    fn nft_metadata(&self) -> NFTMetadata {
+        self.metadata.get().unwrap()
     }
 }
