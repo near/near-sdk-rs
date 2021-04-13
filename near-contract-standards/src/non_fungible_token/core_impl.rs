ce436f712dbfa8d0569730bdb79db33e1a2074ca
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-04-13 14:23:21 -0400

    start straightening out nft/core_impl

--- near-contract-standards/src/non_fungible_token/core_impl.rs
+++ near-contract-standards/src/non_fungible_token/core_impl.rs
@@ -1,14 +1,15 @@
-use crate::non_fungible_token::approval::NonFungibleTokenApproval;
 use crate::non_fungible_token::core::NonFungibleTokenCore;
-use crate::non_fungible_token::enumeration::NonFungibleTokenEnumeration;
+use crate::non_fungible_token::metadata::TokenMetadata;
 use crate::non_fungible_token::resolver::NonFungibleTokenResolver;
+use crate::non_fungible_token::token::{JsonToken, TokenId};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::LookupMap;
-use near_sdk::json_types::{ValidAccountId, U128};
+use near_sdk::collections::{LookupMap, UnorderedMap, UnorderedSet};
+use near_sdk::json_types::{Base64VecU8, ValidAccountId, U128};
 use near_sdk::{
     assert_one_yocto, env, ext_contract, log, AccountId, Balance, Gas, PromiseOrValue,
     PromiseResult, StorageUsage,
 };
+use std::collections::HashMap;
 
 const GAS_FOR_RESOLVE_TRANSFER: Gas = 5_000_000_000_000;
 const GAS_FOR_FT_TRANSFER_CALL: Gas = 25_000_000_000_000 + GAS_FOR_RESOLVE_TRANSFER;
@@ -16,7 +17,7 @@ const GAS_FOR_FT_TRANSFER_CALL: Gas = 25_000_000_000_000 + GAS_FOR_RESOLVE_TRANS
 const NO_DEPOSIT: Balance = 0;
 
 #[ext_contract(ext_self)]
-trait NonFungibleTokenResolver {
+trait NFTResolver {
     fn nft_resolve_transfer(
         &mut self,
         owner_id: AccountId,
@@ -48,99 +49,135 @@ pub trait NonFungibleTokenReceiver {
 /// For example usage, see examples/non-fungible-token/src/lib.rs.
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct NonFungibleToken {
-    pub tokens_per_owner: LookupMap<AccountId, UnorderedSet<TokenId>>,
-
-    pub tokens_by_id: LookupMap<TokenId, Token>,
-
-    // how to include this only in Metadata impl?
-    pub token_metadata_by_id: UnorderedMap<TokenId, TokenMetadata>,
-
+    // owner of contract; this is the only account allowed to call `mint`
     pub owner_id: AccountId,
 
-    // The storage size in bytes for one account.
+    // The storage size in bytes for each new token
     pub extra_storage_in_bytes_per_token: StorageUsage,
+
+    // always required
+    pub owner_by_id: UnorderedMap<TokenId, AccountId>,
+
+    // required by metadata extension
+    pub token_metadata_by_id: Option<LookupMap<TokenId, TokenMetadata>>,
+
+    // required by enumeration extension
+    pub tokens_per_owner: Option<LookupMap<AccountId, UnorderedSet<TokenId>>>,
+
+    // required by approval extension
+    pub approvals_by_id: Option<LookupMap<TokenId, HashMap<AccountId, u64>>>,
+    pub next_approval_id_by_id: Option<LookupMap<TokenId, u64>>,
+}
+
+use near_sdk::BorshStorageKey;
+
+#[derive(BorshStorageKey, BorshSerialize)]
+pub enum StorageKeys {
+    TokensForOwner { account_hash: Vec<u8> },
 }
 
 impl NonFungibleToken {
     pub fn new(
-        tokens_per_owner_prefix: Vec<u8>,
-        tokens_by_id_prefix: Vec<u8>,
-        token_metadata_prefix: Vec<u8>,
-        owner_id: ValidAccountId, // TODO what is this?
+        owner_by_id_prefix: Vec<u8>,
+        owner_id: ValidAccountId,
+        token_metadata_prefix: Option<Vec<u8>>,
+        enumeration_prefix: Option<Vec<u8>>,
+        approval_prefix: Option<Vec<u8>>,
     ) -> Self {
         let mut this = Self {
-            tokens_per_owner: LookupMap::new(tokens_per_owner_prefix),
-            tokens_by_id: LookupMap::new(tokens_by_id_prefix),
-            token_metadata_by_id: UnorderedMap::new(token_metadata_prefix),
             owner_id: owner_id.into(),
             extra_storage_in_bytes_per_token: 0,
+            owner_by_id: UnorderedMap::new(owner_by_id_prefix),
+            token_metadata_by_id: if let Some(prefix) = token_metadata_prefix {
+                Some(LookupMap::new(prefix))
+            } else {
+                None
+            },
+            tokens_per_owner: if let Some(prefix) = enumeration_prefix {
+                Some(LookupMap::new(prefix))
+            } else {
+                None
+            },
+            approvals_by_id: if let Some(prefix) = approval_prefix {
+                Some(LookupMap::new(prefix))
+            } else {
+                None
+            },
+            next_approval_id_by_id: if let Some(prefix) = approval_prefix {
+                Some(LookupMap::new([prefix, "n".into()].concat()))
+            } else {
+                None
+            },
         };
-        this.measure_account_storage_usage();
+        this.measure_min_token_storage_cost();
         this
     }
 
-    fn measure_account_storage_usage(&mut self) {
+    // TODO: does this seem reasonable?
+    fn measure_min_token_storage_cost(&mut self) {
         let initial_storage_usage = env::storage_usage();
-        let tmp_account_id = "a".repeat(64);
-        self.accounts.insert(&tmp_account_id, &0u128);
-        self.account_storage_usage = env::storage_usage() - initial_storage_usage;
-        self.accounts.remove(&tmp_account_id);
-    }
+        let tmp_token_id = "a".repeat(64); // TODO: what's a reasonable max TokenId length?
+        let tmp_owner_id = "a".repeat(64);
 
-    pub fn internal_unwrap_balance_of(&self, account_id: &AccountId) -> Balance {
-        match self.accounts.get(&account_id) {
-            Some(balance) => balance,
-            None => env::panic(format!("The account {} is not registered", &account_id).as_bytes()),
+        // 1. set some dummy data
+        self.owner_by_id.insert(&tmp_token_id, &tmp_owner_id);
+        if let Some(token_metadata_by_id) = self.token_metadata_by_id {
+            token_metadata_by_id.insert(
+                &tmp_token_id,
+                &TokenMetadata {
+                    title: Some("a".repeat(64)),
+                    description: Some("a".repeat(64)),
+                    media: Some("a".repeat(64)),
+                    media_hash: Some(Base64VecU8::from("a".repeat(64).as_bytes().to_vec())),
+                    copies: Some(1),
+                    issued_at: None,
+                    expires_at: None,
+                    starts_at: None,
+                    updated_at: None,
+                    extra: None,
+                    reference: None,
+                    reference_hash: None,
+                },
+            );
         }
-    }
+        if let Some(tokens_per_owner) = self.tokens_per_owner {
+            let token_ids = tokens_per_owner.get(&tmp_owner_id).unwrap_or_else(|| {
+                UnorderedSet::new(StorageKeys::TokensForOwner {
+                    account_hash: env::sha256(tmp_owner_id.as_bytes()),
+                })
+            });
+            token_ids.insert(&tmp_token_id);
+        }
+        if let Some(approvals_by_id) = self.approvals_by_id {
+            let mut approvals = HashMap::new();
+            approvals.insert(tmp_owner_id.clone(), 1u64);
+            approvals_by_id.insert(&tmp_token_id, &approvals);
+        }
+        if let Some(next_approval_id_by_id) = self.next_approval_id_by_id {
+            next_approval_id_by_id.insert(&tmp_token_id, &1u64);
+        }
 
-    pub fn internal_deposit(&mut self, account_id: &AccountId, amount: Balance) {
-        let balance = self.internal_unwrap_balance_of(account_id);
-        if let Some(new_balance) = balance.checked_add(amount) {
-            self.accounts.insert(&account_id, &new_balance);
-            self.total_supply =
-                self.total_supply.checked_add(amount).expect("Total supply overflow");
-        } else {
-            env::panic(b"Balance overflow");
-        }
-    }
+        // 2. see how much space it took
+        self.extra_storage_in_bytes_per_token = env::storage_usage() - initial_storage_usage;
 
-    pub fn internal_withdraw(&mut self, account_id: &AccountId, amount: Balance) {
-        let balance = self.internal_unwrap_balance_of(account_id);
-        if let Some(new_balance) = balance.checked_sub(amount) {
-            self.accounts.insert(&account_id, &new_balance);
-            self.total_supply =
-                self.total_supply.checked_sub(amount).expect("Total supply overflow");
-        } else {
-            env::panic(b"The account doesn't have enough balance");
+        // 3. roll it all back
+        if let Some(next_approval_id_by_id) = self.next_approval_id_by_id {
+            next_approval_id_by_id.remove(&tmp_token_id);
         }
-    }
-
-    pub fn internal_transfer(
-        &mut self,
-        sender_id: &AccountId,
-        receiver_id: &AccountId,
-        amount: Balance,
-        memo: Option<String>,
-    ) {
-        assert_ne!(sender_id, receiver_id, "Sender and receiver should be different");
-        assert!(amount > 0, "The amount should be a positive number");
-        self.internal_withdraw(sender_id, amount);
-        self.internal_deposit(receiver_id, amount);
-        log!("Transfer {} from {} to {}", amount, sender_id, receiver_id);
-        if let Some(memo) = memo {
-            log!("Memo: {}", memo);
+        if let Some(approvals_by_id) = self.approvals_by_id {
+            approvals_by_id.remove(&tmp_token_id);
         }
-    }
-
-    pub fn internal_register_account(&mut self, account_id: &AccountId) {
-        if self.accounts.insert(&account_id, &0).is_some() {
-            env::panic(b"The account is already registered");
+        if let Some(tokens_per_owner) = self.tokens_per_owner {
+            tokens_per_owner.remove(&tmp_owner_id);
         }
+        if let Some(token_metadata_by_id) = self.token_metadata_by_id {
+            token_metadata_by_id.remove(&tmp_token_id);
+        }
+        self.owner_by_id.remove(&tmp_token_id);
     }
 }
 
-impl NonFungibleTokenCore for FungibleToken {
+impl NonFungibleTokenCore for NonFungibleToken {
     fn nft_transfer(&mut self, receiver_id: ValidAccountId, amount: U128, memo: Option<String>) {
         assert_one_yocto();
         let sender_id = env::predecessor_account_id();
@@ -179,12 +216,9 @@ impl NonFungibleTokenCore for FungibleToken {
         .into()
     }
 
-    fn nft_total_supply(&self) -> U128 {
-        self.total_supply.into()
-    }
-
-    fn nft_balance_of(&self, account_id: ValidAccountId) -> U128 {
-        self.accounts.get(account_id.as_ref()).unwrap_or(0).into()
+    fn nft_token(self, token_id: TokenId) -> JsonToken {
+        // self.accounts.get(account_id.as_ref()).unwrap_or(0).into()
+        JsonToken {}
     }
 }
 
@@ -236,7 +270,7 @@ impl NonFungibleToken {
     }
 }
 
-impl NonFungibleTokenResolver for FungibleToken {
+impl NonFungibleTokenResolver for NonFungibleToken {
     fn nft_resolve_transfer(
         &mut self,
         sender_id: ValidAccountId,
