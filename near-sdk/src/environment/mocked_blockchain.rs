6375cc5b2c7fa1de3ec0d249e83c4451eb790de6
austinabell <austinabell8@gmail.com>
2021-05-11 19:20:51 -0400

    fmt

--- near-sdk/src/environment/mocked_blockchain.rs
+++ near-sdk/src/environment/mocked_blockchain.rs
@@ -1,8 +1,8 @@
 use crate::environment::blockchain_interface::BlockchainInterface;
+use crate::types::{AccountId, Balance, PromiseResult};
 use crate::RuntimeFeesConfig;
 use near_vm_logic::mocks::mock_external::{MockedExternal, Receipt};
 use near_vm_logic::mocks::mock_memory::MockedMemory;
-use crate::types::{AccountId, Balance, PromiseResult};
 use near_vm_logic::{External, MemoryLike, VMConfig, VMContext, VMLogic, VMOutcome};
 use std::cell::RefCell;
 use std::collections::HashMap;
--- near-sdk/src/types/mod.rs
+++ near-sdk/src/types/mod.rs
@@ -12,4 +12,3 @@ pub type Timestamp = u64;
 
 /// Raw type for 32 bytes of the hash.
 pub type CryptoHash = [u8; 32];
-
