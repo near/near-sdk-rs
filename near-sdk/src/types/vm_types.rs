c2aed6f836839a094b3eb8dd5ba678f90e7af159
austinabell <austinabell8@gmail.com>
2021-05-11 19:15:02 -0400

    reorder for consistency

--- near-sdk/src/types/vm_types.rs
+++ near-sdk/src/types/vm_types.rs
@@ -1,9 +1,9 @@
+// TODO restrict to non-wasm32
+pub use near_vm_logic::types::{PromiseResult, ReturnData};
+
 //* Types from near_vm_logic
 
 pub type PublicKey = Vec<u8>;
 pub type PromiseIndex = u64;
 pub type ReceiptIndex = u64;
 pub type IteratorIndex = u64;
-
-// TODO restrict to non-wasm32
-pub use near_vm_logic::types::{PromiseResult, ReturnData};
