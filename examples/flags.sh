3d7c4807471ae7e6e1f385fa428e8ba927511a8f
Mike Purvis <mikedotexe@gmail.com>
2021-07-19 18:09:22 -0700

    for examples change RUSTFLAGS to include remap-path-prefix flag

--- examples/flags.sh
+++ examples/flags.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$KEEP_NAMES" ]; then
-  export RUSTFLAGS='-C link-arg=-s'
+  export RUSTFLAGS="-C link-arg=-s --remap-path-prefix $PWD=/pwd --remap-path-prefix $CARGO_HOME=/cargo_home"
 else
   export RUSTFLAGS=''
 fi
