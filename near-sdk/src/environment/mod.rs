4b849781c0b5fd222c302ac26662a9b03f1df8bf
austinabell <austinabell8@gmail.com>
2021-07-24 16:47:52 -0400

    fix annotation from merge

--- near-sdk/src/environment/mod.rs
+++ near-sdk/src/environment/mod.rs
@@ -4,6 +4,7 @@ pub mod sys;
 #[cfg(feature = "unstable")]
 pub mod hash;
 
+#[cfg(not(target_arch = "wasm32"))]
 /// Mock blockchain utilities. These can only be used inside tests and are not available for
 /// a wasm32 target.
 pub mod mock;
