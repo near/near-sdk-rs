c8258d58de727d1191d016303a6ec0bce950acff
austinabell <austinabell8@gmail.com>
2021-07-29 15:51:50 -0400

    feat: setup docker builds for contracts and add WASM check to CI

---
+++ examples/build_docker.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Exit script as soon as a command fails.
+set -ex
+
+NAME="$1"
+IDENT=${NAME#*/}
+echo "$IDENT"
+
+if docker ps -a --format '{{.Names}}' | grep -Eq "^build_${IDENT}\$"; then
+    echo "Container exists"
+else
+docker create \
+     --mount type=bind,source=$(pwd),target=/host \
+     --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+     --name=build_$IDENT \
+     -w /host/$NAME \
+     -e RUSTFLAGS='-C link-arg=-s' \
+     -it nearprotocol/contract-builder \
+     /bin/bash
+fi
+
+docker start build_$IDENT
+docker exec build_$IDENT /bin/bash -c "./build.sh"
+
+# mkdir -p res
+# cp $NAME/target/wasm32-unknown-unknown/release/$CONTRACT_WASM_NAME.wasm $NAME/res/$CONTRACT_WASM_NAME.wasm
