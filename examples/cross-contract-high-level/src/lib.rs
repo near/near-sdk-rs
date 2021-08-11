19ad8d34a0738eeb61b4fa06c5ca395db94de563
Austin Abell <austinabell8@gmail.com>
2021-08-11 13:59:36 -0400

    Update examples/cross-contract-high-level/src/lib.rs

--- examples/cross-contract-high-level/src/lib.rs
+++ examples/cross-contract-high-level/src/lib.rs
@@ -53,7 +53,6 @@ impl CrossContract {
 
     #[result_serializer(borsh)]
     pub fn merge_sort(&self, arr: Vec<u8>) -> PromiseOrValue<Vec<u8>> {
-        env::log_str("merge_sort");
         if arr.len() <= 1 {
             return PromiseOrValue::Value(arr);
         }
