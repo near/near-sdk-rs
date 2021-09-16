932d1497c4a78cbc1d515b2bd981f575b272b54f
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-09-15 21:33:25 -0400

    feat: add near-runner-jest to fungible-token
    This adds one test to ensure that everything is configured properly.
    
    It feels like there is too much JS config in the examples/fungible-token
    directory, with how near-runner-jest is currently configured.

---
+++ examples/fungible-token/__tests__/main.spec.ts
@@ -0,0 +1,28 @@
+import path from 'path';
+import { Runner } from 'near-runner-jest';
+
+const TOTAL_SUPPLY = '1000000';
+
+const runner = Runner.create(async ({ root }) => {
+  const alice = await root.createAccount('alice');
+  const ft = await root.createAndDeploy(
+    'ft',
+    path.join(__dirname, '..', 'res', 'fungible_token.wasm'),
+  );
+
+  await root.call(
+    ft,
+    'new_default_meta',
+    {
+      owner_id: root,
+      total_supply: TOTAL_SUPPLY,
+    }
+  );
+
+  return { alice, ft };
+});
+
+runner.test('simple transfer', async ({ ft }) => {
+  const totalSupply: string = await ft.view('ft_total_supply');
+  expect(totalSupply).toBe(TOTAL_SUPPLY);
+});
