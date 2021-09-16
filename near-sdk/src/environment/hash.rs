34b75f56ee6dda7c83859e8c27dbdea2d2ac74fb
austinabell <austinabell8@gmail.com>
2021-09-16 19:53:42 -0400

    seal cryptohash trait

--- near-sdk/src/environment/hash.rs
+++ near-sdk/src/environment/hash.rs
@@ -8,8 +8,16 @@ fn read_register_fixed(register_id: u64, buf: &mut [MaybeUninit<u8>]) {
     unsafe { sys::read_register(register_id, buf.as_ptr() as _) }
 }
 
+mod private {
+    /// Seal `CryptoHasher` implementations to limit usage to the builtin implementations
+    pub trait Sealed {}
+
+    impl Sealed for super::Sha256 {}
+    impl Sealed for super::Keccak256 {}
+}
+
 /// Cryptographic hashes that can be used within the SDK as a hashing function.
-pub trait CryptoHasher {
+pub trait CryptoHasher: self::private::Sealed {
     /// Output type of the hashing function.
     type Digest;
 
