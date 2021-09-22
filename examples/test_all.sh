ac9aaf0bf3809288eb61e2a5006bb48c402465b2
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-09-22 14:58:17 -0400

    build: rm near-runner tests from CI test_all.sh run

--- examples/test_all.sh
+++ examples/test_all.sh
@@ -3,16 +3,20 @@ set -e
 
 pushd $(dirname ${BASH_SOURCE[0]})
 
-echo "Installing near-runner-jest"
-yarn install
-echo ""
+if [[ $@ != *"--no-js"* ]]; then
+    echo "Installing near-runner-jest"
+    yarn install
+    echo ""
+fi
 
 for d in */Cargo.toml ; do
     d=$(dirname "$d");
     echo "Testing $d";
     (cd $d && cargo test --workspace -- --nocapture)
-    if [ -d "$d/__tests__" ]; then
-        (cd $d && yarn near-runner-jest)
+    if [[ $@ != *"--no-js"* ]]; then
+        if [ -d "$d/__tests__" ]; then
+            (cd $d && yarn near-runner-jest)
+        fi
     fi
 done
 
