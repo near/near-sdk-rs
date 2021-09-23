7afa67e6196692e053d4ff6565a996b75fa31042
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-09-23 14:46:32 -0400

    build: test_all runs only cargo tests by default
    * add help output when given `--help` or `-h`
    * only install JS dependencies & run JS tests if given `--with-js` flag

--- ci-test.sh
+++ ci-test.sh
@@ -2,7 +2,7 @@
 
 if [[ "${NEAR_RELEASE}" == "true" ]]; then
     echo "Test with release version of borsh and near-vm-logic"
-    sed -n '/^borsh/p' near-sdk/Cargo.toml
+    sed -n '/^borsh/p' near-sdk/Cargo.toml 
     sed -n '/^near-vm-logic/p' near-sdk/Cargo.toml
     cargo test --all
 else
@@ -14,7 +14,7 @@ else
     cp Cargo.lock{,.bak}
 
     sed -i "" "s|###||g" Cargo.toml
-
+    
     set +e
     cargo test --all
     status=$?
@@ -31,8 +31,8 @@ else
 
     ./examples/build_all_docker.sh --check
     echo "Testing all examples"
-    ./examples/test_all.sh --no-js
-    # TODO re-enable when parsing unknown sections is fixed in twiggy
+    ./examples/test_all.sh
+    # TODO re-enable when parsing unknown sections is fixed in twiggy 
     #      https://github.com/rustwasm/twiggy/pull/576
     # echo "Checking size of all example contracts"
     # ./examples/size_all.sh
--- examples/test_all.sh
+++ examples/test_all.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 set -e
 
+if [[ $@ == *"--help"* || $@ == *"-h"* ]]; then
+    echo "Run tests in all examples folders. Examples:"
+    echo ""
+    echo "    ./test_all.sh            # run only cargo tests"
+    echo "    ./test_all.sh --with-js  # also run near-runner tests (requires NodeJS & Yarn)"
+    echo ""
+    exit 0
+fi
+
 pushd $(dirname ${BASH_SOURCE[0]})
 
-if [[ $@ != *"--no-js"* ]]; then
+if [[ $@ == *"--with-js"* ]]; then
     echo "Installing near-runner-jest"
     yarn install
     echo ""
@@ -13,7 +22,7 @@ for d in */Cargo.toml ; do
     d=$(dirname "$d");
     echo "Testing $d";
     (cd $d && cargo test --workspace -- --nocapture)
-    if [[ $@ != *"--no-js"* ]]; then
+    if [[ $@ == *"--with-js"* ]]; then
         if [ -d "$d/__tests__" ]; then
             (cd $d && yarn near-runner-jest)
         fi
