fa607bda85470104a8bd58b6544efebf11d58228
Esraa Jbara <jbesraa@gmail.com>
2021-09-23 11:19:08 +0300

    add pausable_ft macro

--- near-contract-standards/src/fungible_token/macros.rs
+++ near-contract-standards/src/fungible_token/macros.rs
@@ -14,7 +14,7 @@ macro_rules! impl_fungible_token_core {
                 receiver_id: AccountId,
                 amount: U128,
                 memo: Option<String>,
-            ) {
+                ) {
                 self.$token.ft_transfer(receiver_id, amount, memo)
             }
 
@@ -25,7 +25,7 @@ macro_rules! impl_fungible_token_core {
                 amount: U128,
                 memo: Option<String>,
                 msg: String,
-            ) -> PromiseOrValue<U128> {
+                ) -> PromiseOrValue<U128> {
                 self.$token.ft_transfer_call(receiver_id, amount, memo, msg)
             }
 
@@ -46,7 +46,7 @@ macro_rules! impl_fungible_token_core {
                 sender_id: AccountId,
                 receiver_id: AccountId,
                 amount: U128,
-            ) -> U128 {
+                ) -> U128 {
                 let (used_amount, burned_amount) =
                     self.$token.internal_ft_resolve_transfer(&sender_id, receiver_id, amount);
                 if burned_amount > 0 {
@@ -77,7 +77,7 @@ macro_rules! impl_fungible_token_storage {
                 &mut self,
                 account_id: Option<AccountId>,
                 registration_only: Option<bool>,
-            ) -> StorageBalance {
+                ) -> StorageBalance {
                 self.$token.storage_deposit(account_id, registration_only)
             }
 
@@ -91,7 +91,7 @@ macro_rules! impl_fungible_token_storage {
                 #[allow(unused_variables)]
                 if let Some((account_id, balance)) = self.$token.internal_storage_unregister(force) {
                     $(self.$on_account_closed_fn(account_id, balance);)?
-                    true
+                        true
                 } else {
                     false
                 }
@@ -103,6 +103,80 @@ macro_rules! impl_fungible_token_storage {
 
             fn storage_balance_of(&self, account_id: AccountId) -> Option<StorageBalance> {
                 self.$token.storage_balance_of(account_id)
+            }
+        }
+    };
+}
+
+/// Base fungible token + pausable functionality
+#[macro_export]
+macro_rules! impl_pausable_fungible_token_core {
+    ($contract: ident, $token: ident $(, $on_tokens_burned_fn:ident)?) => {
+        use $crate::fungible_token::core::FungibleTokenCore;
+        use $crate::fungible_token::resolver::FungibleTokenResolver;
+        use $crate::fungible_token::pausable::Pausable;
+
+        #[near_bindgen]
+        impl FungibleTokenCore for $contract {
+            #[payable]
+            fn ft_transfer(
+                &mut self,
+                receiver_id: AccountId,
+                amount: U128,
+                memo: Option<String>,
+                ) {
+                assert!(!self.paused, "Contract Is Paused");
+                self.$token.ft_transfer(receiver_id, amount, memo)
+            }
+
+            #[payable]
+            fn ft_transfer_call(
+                &mut self,
+                receiver_id: AccountId,
+                amount: U128,
+                memo: Option<String>,
+                msg: String,
+                ) -> PromiseOrValue<U128> {
+                assert!(!self.paused, "Contract Is Paused");
+                self.$token.ft_transfer_call(receiver_id, amount, memo, msg)
+            }
+
+            fn ft_total_supply(&self) -> U128 {
+                self.$token.ft_total_supply()
+            }
+
+            fn ft_balance_of(&self, account_id: AccountId) -> U128 {
+                self.$token.ft_balance_of(account_id)
+            }
+        }
+
+        #[near_bindgen]
+        impl Pausable for $contract {
+            fn pause(&mut self, p: bool) {
+                self.paused = p;
+            }
+
+
+            fn is_paused(&self) -> bool {
+                self.paused
+            }
+        }
+
+        #[near_bindgen]
+        impl FungibleTokenResolver for $contract {
+            #[private]
+            fn ft_resolve_transfer(
+                &mut self,
+                sender_id: AccountId,
+                receiver_id: AccountId,
+                amount: U128,
+                ) -> U128 {
+                let (used_amount, burned_amount) =
+                    self.$token.internal_ft_resolve_transfer(&sender_id, receiver_id, amount);
+                if burned_amount > 0 {
+                    $(self.$on_tokens_burned_fn(sender_id, burned_amount);)?
+                }
+                used_amount.into()
             }
         }
     };
