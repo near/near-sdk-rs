2501024f51621c01ebcc84d9b30b7de697bab8c3
Constantin Dogaru <constantin.dogaru@mbntech.ro>
2021-09-29 14:32:00 +0300

    add new test for ownable

--- near-contract-standards/src/access/mod.rs
+++ near-contract-standards/src/access/mod.rs
@@ -10,7 +10,10 @@ pub struct Ownable {
 
 impl Ownable {
     pub fn new() -> Self {
-        Self { owner: env::predecessor_account_id(), null_owner: "".parse::<AccountId>().unwrap() }
+        Self {
+            owner: env::predecessor_account_id(),
+            null_owner: "0000000000".parse::<AccountId>().unwrap(),
+        }
     }
 
     pub fn owner(&self) -> AccountId {
@@ -112,5 +115,30 @@ impl AccessControl {
             );
         }
         self.roles.get_mut(&role).unwrap().admin_role = admin_role;
+    }
+}
+
+#[cfg(all(test, not(target_arch = "wasm32")))]
+mod tests {
+    use near_sdk::test_utils::{accounts, VMContextBuilder};
+    use near_sdk::testing_env;
+
+    use super::*;
+
+    fn get_context(predecessor_account_id: AccountId) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder
+            .current_account_id(accounts(0))
+            .signer_account_id(predecessor_account_id.clone())
+            .predecessor_account_id(predecessor_account_id);
+        builder
+    }
+
+    #[test]
+    fn test_new() {
+        let context = get_context(accounts(1));
+        testing_env!(context.build());
+        let ownable = Ownable::new();
+        assert_eq!(ownable.owner(), accounts(1));
     }
 }
