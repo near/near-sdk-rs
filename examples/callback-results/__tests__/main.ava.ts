24fe49ca82fdd4627efaf6d18c3ae957c3f591cb
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2021-09-29 09:04:54 -0400

    chore: clean up callback_results runner tests

--- examples/callback-results/__tests__/main.ava.ts
+++ examples/callback-results/__tests__/main.ava.ts
@@ -17,12 +17,14 @@ runner.test('method `a` always returns `8`', async (t, { root, contract }) => {
 });
 
 [
-  { fail_b: false, c_value: 1, expected: [false, false] },
-  { fail_b: true, c_value: 1, expected: [true, false] },
-  { fail_b: false, c_value: 0, expected: [false, true] },
-  { fail_b: true, c_value: 0, expected: [true, true] },
-].forEach(({ fail_b, c_value, expected }) => {
-  runner.test(`call_all with {fail_b: ${fail_b}, c_value: ${c_value}} gives ${expected}`, async (t, { root, contract }) => {
+  [false, 1, [false, false]],
+  [true, 1, [true, false]],
+  [false, 0, [false, true]],
+  [true, 0, [true, true]],
+].forEach(([fail_b, c_value, expected]) => {
+  const args = JSON.stringify({ fail_b, c_value });
+  const out = JSON.stringify(expected);
+  runner.test(`call_all(${args}) == ${out}`, async (t, { root, contract }) => {
     t.deepEqual(
       await root.call(contract, 'call_all', { fail_b, c_value }, {
         gas: Gas.parse('240Tgas')
