cfe439e1b415f476fa1cae9225e2f679c22261d7
Constantin Dogaru <constantin.dogaru@mbntech.ro>
2021-10-07 16:59:38 +0300

    add documentation in the mod file

--- near-contract-standards/src/access/mod.rs
+++ near-contract-standards/src/access/mod.rs
@@ -1,3 +1,7 @@
 /// Based on openzeppelin/access. See https://github.com/OpenZeppelin/openzeppelin-contracts/tree/master/contracts/access.
+
+/// Contract module that allows accounts to implement role-based access control mechanisms.
 pub mod access_control;
+/// Contract module which provides a basic access control mechanism, where there is an account (owner)
+/// that can be granted exclusive access to specific functions.
 pub mod ownable;
