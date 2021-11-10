2dd8688446f892c8d4673784afb63e3010b128fc
Constantin Dogaru <constantin.dogaru@mbntech.ro>
2021-11-10 23:14:57 +0200

    add missing imports

--- near-contract-standards/src/storage_management/mod.rs
+++ near-contract-standards/src/storage_management/mod.rs
@@ -1,5 +1,6 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::json_types::U128;
-use near_sdk::serde::Serialize;
+use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::AccountId;
 
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
