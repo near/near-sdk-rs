b84832dcdba17f2ab480b9d53e8b7f1969cc8534
austinabell <austinabell8@gmail.com>
2021-11-17 11:34:07 +0000

    reorganize code

--- near-sdk/src/environment/hash.rs
+++ near-sdk/src/environment/hash.rs
@@ -30,13 +30,13 @@ impl CryptoHasher for Sha256 {
     type Digest = [u8; 32];
 
     fn hash(ingest: &[u8]) -> Self::Digest {
-        unsafe { sys::sha256(ingest.len() as _, ingest.as_ptr() as _, ATOMIC_OP_REGISTER) };
-
-        let mut hash = [MaybeUninit::<u8>::uninit(); 32];
         //* SAFETY: sha256 syscall will always generate 32 bytes inside of the atomic op register
         //*         so the read will have a sufficient buffer of 32, and can transmute from uninit
         //*         because all bytes are filled. This assumes a valid sha256 implementation.
         unsafe {
+            sys::sha256(ingest.len() as _, ingest.as_ptr() as _, ATOMIC_OP_REGISTER);
+
+            let mut hash = [MaybeUninit::<u8>::uninit(); 32];
             sys::read_register(ATOMIC_OP_REGISTER, hash.as_mut_ptr() as _);
             std::mem::transmute(hash)
         }
@@ -52,13 +52,13 @@ impl CryptoHasher for Keccak256 {
     type Digest = [u8; 32];
 
     fn hash(ingest: &[u8]) -> Self::Digest {
-        unsafe { sys::keccak256(ingest.len() as _, ingest.as_ptr() as _, ATOMIC_OP_REGISTER) };
-
-        let mut hash = [MaybeUninit::<u8>::uninit(); 32];
         //* SAFETY: keccak256 syscall will always generate 32 bytes inside of the atomic op register
         //*         so the read will have a sufficient buffer of 32, and can transmute from uninit
         //*         because all bytes are filled. This assumes a valid keccak256 implementation.
         unsafe {
+            sys::keccak256(ingest.len() as _, ingest.as_ptr() as _, ATOMIC_OP_REGISTER);
+
+            let mut hash = [MaybeUninit::<u8>::uninit(); 32];
             sys::read_register(ATOMIC_OP_REGISTER, hash.as_mut_ptr() as _);
             std::mem::transmute(hash)
         }
