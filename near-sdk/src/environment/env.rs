a8212a7ff1120f79c284668a89a529ddf35a95b0
Daniyar Itegulov <ditegulov@gmail.com>
2021-11-29 21:06:08 +1100

    Implement safe API for RIPEMD-160

--- near-sdk/src/environment/env.rs
+++ near-sdk/src/environment/env.rs
@@ -54,6 +54,12 @@ macro_rules! method_into_register {
 //* Note: need specific length functions because const generics don't work with mem::transmute
 //* https://github.com/rust-lang/rust/issues/61956
 
+pub(crate) unsafe fn read_register_fixed_20(register_id: u64) -> [u8; 20] {
+    let mut hash = [MaybeUninit::<u8>::uninit(); 20];
+    sys::read_register(register_id, hash.as_mut_ptr() as _);
+    std::mem::transmute(hash)
+}
+
 pub(crate) unsafe fn read_register_fixed_32(register_id: u64) -> [u8; 32] {
     let mut hash = [MaybeUninit::<u8>::uninit(); 32];
     sys::read_register(register_id, hash.as_mut_ptr() as _);
@@ -276,6 +282,17 @@ pub fn keccak512_array(value: &[u8]) -> [u8; 64] {
     unsafe {
         sys::keccak512(value.len() as _, value.as_ptr() as _, ATOMIC_OP_REGISTER);
         read_register_fixed_64(ATOMIC_OP_REGISTER)
+    }
+}
+
+/// Hashes the bytes using the RIPEMD-160 hash function. This returns a 20 byte hash.
+pub fn ripemd160_array(value: &[u8]) -> [u8; 20] {
+    //* SAFETY: ripemd160 syscall will always generate 20 bytes inside of the atomic op register
+    //*         so the read will have a sufficient buffer of 20, and can transmute from uninit
+    //*         because all bytes are filled. This assumes a valid ripemd160 implementation.
+    unsafe {
+        sys::ripemd160(value.len() as _, value.as_ptr() as _, ATOMIC_OP_REGISTER);
+        read_register_fixed_20(ATOMIC_OP_REGISTER)
     }
 }
 
@@ -788,6 +805,11 @@ mod tests {
             )
             .unwrap()
             .as_slice()
+        );
+
+        assert_eq!(
+            &super::ripemd160_array(b"some value"),
+            base64::decode("CfAl/tcE4eysj4iyvaPlaHbaA6w=").unwrap().as_slice()
         );
     }
 }
