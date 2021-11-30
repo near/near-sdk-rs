c08a50229d70dd0b6c37f42ebd6ef70d13b87da3
Austin Abell <austinabell8@gmail.com>
2021-11-30 17:44:12 -0500

    make slight changes to fix format (#653)

--- near-contract-standards/src/non_fungible_token/events.rs
+++ near-contract-standards/src/non_fungible_token/events.rs
@@ -1,15 +1,20 @@
+use std::fmt;
+
 use near_sdk::serde::{Deserialize, Serialize};
 
-//enum that represents the data type of the EventLogJson. 
+//enum that represents the data type of the EventLogJson.
 //the enum can either be an NftMintLog or an NftTransferLog
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "event", content = "data")]
+#[serde(rename_all = "snake_case")]
 #[serde(crate = "near_sdk::serde")]
-pub enum EventLogJsonData {
-    NftMintLog(Vec<NftMintLog>),
-    NftTransferLog(Vec<NftTransferLog>),
+#[non_exhaustive]
+pub enum EventLogVariant {
+    NftMint(Vec<NftMintLog>),
+    NftTransfer(Vec<NftTransferLog>),
 }
 
-// Interface to capture data 
+// Interface to capture data
 // about an event
 // Arguments
 // * `standard`: name of standard e.g. nep171
@@ -18,11 +23,20 @@ pub enum EventLogJsonData {
 // * `data`: associate event data
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(crate = "near_sdk::serde")]
-pub struct EventLogJson {
-    pub standard: String, 
-    pub version: String, 
-    pub event: String,
-    pub data: EventLogJsonData
+pub struct EventLog {
+    pub standard: String,
+    pub version: String,
+    #[serde(flatten)]
+    pub event: EventLogVariant,
+}
+
+impl fmt::Display for EventLog {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_fmt(format_args!(
+            "EVENT_JSON:{}",
+            &serde_json::to_string(self).map_err(|_| fmt::Error)?
+        ))
+    }
 }
 
 // An event log to capture token minting
@@ -33,12 +47,12 @@ pub struct EventLogJson {
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(crate = "near_sdk::serde")]
 pub struct NftMintLog {
-    pub owner_id: String, 
-    pub token_ids: Vec<String>, 
+    pub owner_id: String,
+    pub token_ids: Vec<String>,
 
     //Only serialize if the option is not none
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub memo: Option<String>
+    pub memo: Option<String>,
 }
 
 // An event log to capture token transfer
@@ -53,11 +67,38 @@ pub struct NftMintLog {
 pub struct NftTransferLog {
     //Only serialize if the option is not none
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub authorized_id: Option<String>, 
-    pub old_owner_id: String, 
-    pub new_owner_id: String, 
-    pub token_ids: Vec<String>, 
+    pub authorized_id: Option<String>,
+    pub old_owner_id: String,
+    pub new_owner_id: String,
+    pub token_ids: Vec<String>,
     //Only serialize if the option is not none
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub memo: Option<String>
+    pub memo: Option<String>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn nep_format_vector() {
+        let expected = r#"EVENT_JSON:{"standard":"nep171","version":"1.0.0","event":"nft_mint","data":[{"owner_id":"foundation.near","token_ids":["aurora","proximitylabs"]},{"owner_id":"user1.near","token_ids":["meme"]}]}"#;
+        let log = EventLog {
+            standard: "nep171".to_string(),
+            version: "1.0.0".to_string(),
+            event: EventLogVariant::NftMint(vec![
+                NftMintLog {
+                    owner_id: "foundation.near".to_owned(),
+                    token_ids: vec!["aurora".to_string(), "proximitylabs".to_string()],
+                    memo: None,
+                },
+                NftMintLog {
+                    owner_id: "user1.near".to_owned(),
+                    token_ids: vec!["meme".to_string()],
+                    memo: None,
+                },
+            ]),
+        };
+        assert_eq!(expected, log.to_string());
+    }
 }
