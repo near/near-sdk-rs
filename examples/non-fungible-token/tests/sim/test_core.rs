740a5940bf5f23c7e52a498e4b9778cd74e9b066
Willem Wyndham <wyndham@cs.unc.edu>
2022-01-13 11:13:41 -0500

    feat: add test to show that gas check is sufficient

--- examples/non-fungible-token/tests/sim/test_core.rs
+++ examples/non-fungible-token/tests/sim/test_core.rs
@@ -127,7 +127,7 @@ fn simulate_transfer_call_receiver_panics() {
 }
 
 #[test]
-fn simulate_transfer_call_receiver_panics_and_produces_two_logs() {
+fn simulate_transfer_call_receiver_panics_and_nft_resolve_transfer_produces_log() {
     let (root, nft, _, receiver, _) = init();
     let args = json!({
       "receiver_id": receiver.account_id(),
@@ -139,11 +139,34 @@ fn simulate_transfer_call_receiver_panics_and_produces_two_logs() {
     let args = serde_json::to_vec(&args).unwrap();
     let res = root
         .create_transaction(nft.account_id())
-        .function_call("nft_transfer_call".to_string(), args, 200_000_000_000_000, 1)
+        .function_call("nft_transfer_call".to_string(), args, 35_000_000_000_000 + 1, 1)
         .submit();
 
     // Prints final log
     assert_eq!(res.logs().len(), 1);
+
+    let token: Token = view!(nft.nft_token(TOKEN_ID.into())).unwrap_json();
+    assert_eq!(token.owner_id, root.account_id());
+}
+
+#[test]
+fn simulate_transfer_call_receiver_panics_and_nft_resolve_transfer_produces_no_log_if_not_enough_gas() {
+    let (root, nft, _, receiver, _) = init();
+    let args = json!({
+      "receiver_id": receiver.account_id(),
+      "token_id": TOKEN_ID,
+      "memo": Some("transfer & call"),
+      "msg": "incorrect message"
+
+    });
+    let args = serde_json::to_vec(&args).unwrap();
+    let res = root
+        .create_transaction(nft.account_id())
+        .function_call("nft_transfer_call".to_string(), args, 35_000_000_000_000 + 1, 1)
+        .submit();
+
+    // Prints final log
+    assert_eq!(res.logs().len(), 0);
 
     let token: Token = view!(nft.nft_token(TOKEN_ID.into())).unwrap_json();
     assert_eq!(token.owner_id, root.account_id());
