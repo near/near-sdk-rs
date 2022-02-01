0e89031054b43d7dd96fe64d85863efc8f202e46
Daniyar Itegulov <ditegulov@gmail.com>
2022-02-01 12:41:28 +1100

    feat(contract-standards): add events to FT contract standard (#723)

--- near-contract-standards/src/event.rs
+++ near-contract-standards/src/event.rs
@@ -7,6 +7,7 @@ use serde::Serialize;
 #[serde(rename_all = "snake_case")]
 pub(crate) enum NearEvent<'a> {
     Nep171(crate::non_fungible_token::events::Nep171Event<'a>),
+    Nep141(crate::fungible_token::events::Nep141Event<'a>),
 }
 
 impl<'a> NearEvent<'a> {
