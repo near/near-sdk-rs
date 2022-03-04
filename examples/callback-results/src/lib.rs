865c02cbf5848f2e88e7fd76807c9eb2b6f900fd
Daniyar Itegulov <ditegulov@gmail.com>
2022-03-03 22:44:09 -0800

    fix: handle callbacks from functions without a return type in a special way (#738)

--- examples/callback-results/src/lib.rs
+++ examples/callback-results/src/lib.rs
@@ -12,21 +12,24 @@ pub trait ExtCrossContract {
     fn a() -> Promise;
     fn b(fail: bool) -> &'static str;
     fn c(value: u8) -> u8;
+    fn d(value: u8);
     fn handle_callbacks(
         #[callback_unwrap] a: u8,
         #[callback_result] b: Result<String, PromiseError>,
         #[callback_result] c: Result<u8, PromiseError>,
+        #[callback_result] d: Result<(), PromiseError>,
     ) -> (bool, bool, bool);
 }
 
 #[near_bindgen]
 impl Callback {
     /// Call functions a, b, and c asynchronously and handle results with `handle_callbacks`.
-    pub fn call_all(fail_b: bool, c_value: u8) -> Promise {
-        let gas_per_promise = env::prepaid_gas() / 5;
+    pub fn call_all(fail_b: bool, c_value: u8, d_value: u8) -> Promise {
+        let gas_per_promise = env::prepaid_gas() / 7;
         ext::a(env::current_account_id(), 0, gas_per_promise)
             .and(ext::b(fail_b, env::current_account_id(), 0, gas_per_promise))
             .and(ext::c(c_value, env::current_account_id(), 0, gas_per_promise))
+            .and(ext::d(d_value, env::current_account_id(), 0, gas_per_promise))
             .then(ext::handle_callbacks(env::current_account_id(), 0, gas_per_promise))
     }
 
@@ -51,18 +54,25 @@ impl Callback {
         value
     }
 
+    /// Panics if value is 0.
+    #[private]
+    pub fn d(value: u8) {
+        require!(value > 0, "Value must be positive");
+    }
+
     /// Receives the callbacks from the other promises called.
     #[private]
     pub fn handle_callbacks(
         #[callback_unwrap] a: u8,
         #[callback_result] b: Result<String, PromiseError>,
         #[callback_result] c: Result<u8, PromiseError>,
-    ) -> (bool, bool) {
+        #[callback_result] d: Result<(), PromiseError>,
+    ) -> (bool, bool, bool) {
         require!(a == A_VALUE, "Promise returned incorrect value");
         if let Ok(s) = b.as_ref() {
             require!(s == "Some string");
         }
-        (b.is_err(), c.is_err())
+        (b.is_err(), c.is_err(), d.is_err())
     }
 }
 
@@ -89,38 +99,56 @@ mod tests {
         // No failures
         let res = contract
             .call(&worker, "call_all")
-            .args_json((false, 1u8))?
+            .args_json((false, 1u8, 1u8))?
             .gas(300_000_000_000_000)
             .transact()
             .await?;
-        assert_eq!(res.json::<(bool, bool)>()?, (false, false));
+        assert_eq!(res.json::<(bool, bool, bool)>()?, (false, false, false));
 
         // Fail b
         let res = contract
             .call(&worker, "call_all")
-            .args_json((true, 1u8))?
+            .args_json((true, 1u8, 1u8))?
             .gas(300_000_000_000_000)
             .transact()
             .await?;
-        assert_eq!(res.json::<(bool, bool)>()?, (true, false));
+        assert_eq!(res.json::<(bool, bool, bool)>()?, (true, false, false));
 
         // Fail c
         let res = contract
             .call(&worker, "call_all")
-            .args_json((false, 0u8))?
+            .args_json((false, 0u8, 1u8))?
             .gas(300_000_000_000_000)
             .transact()
             .await?;
-        assert_eq!(res.json::<(bool, bool)>()?, (false, true));
+        assert_eq!(res.json::<(bool, bool, bool)>()?, (false, true, false));
 
-        // Fail both b and c
+        // Fail d
         let res = contract
             .call(&worker, "call_all")
-            .args_json((true, 0u8))?
+            .args_json((false, 1u8, 0u8))?
             .gas(300_000_000_000_000)
             .transact()
             .await?;
-        assert_eq!(res.json::<(bool, bool)>()?, (true, true));
+        assert_eq!(res.json::<(bool, bool, bool)>()?, (false, false, true));
+
+        // Fail b and c
+        let res = contract
+            .call(&worker, "call_all")
+            .args_json((true, 0u8, 1u8))?
+            .gas(300_000_000_000_000)
+            .transact()
+            .await?;
+        assert_eq!(res.json::<(bool, bool, bool)>()?, (true, true, false));
+
+        // Fail all
+        let res = contract
+            .call(&worker, "call_all")
+            .args_json((true, 0u8, 0u8))?
+            .gas(300_000_000_000_000)
+            .transact()
+            .await?;
+        assert_eq!(res.json::<(bool, bool, bool)>()?, (true, true, true));
 
         Ok(())
     }
