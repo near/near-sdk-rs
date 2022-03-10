4a289b26df12a4b43b7f2ce9735c95ba6aaddade
Anshal savla <anshalsavla@Anshals-MacBook-Pro.local>
2022-03-10 13:30:06 -0800

    Updating build.sh to build multi arch docker images

--- contract-builder/build.sh
+++ contract-builder/build.sh
@@ -1,2 +1,23 @@
-#!/bin/sh
-docker build -t contract-builder .
+#!/bin/bash
+set -eox pipefail
+
+branch=${BUILDKITE_BRANCH//:/_}
+branch=${branch//\//_}
+commit=${BUILDKITE_COMMIT}
+if [[ ${commit} == "HEAD" ]]; then
+    commit=$(git rev-parse HEAD)
+fi
+
+image_name="contract-builder"
+
+if docker buildx ls| grep -q contract-builder;then
+    docker buildx use --builder contract-builder
+else
+    docker buildx create --name contract-builder --use 
+fi
+
+if [[ ${branch} == "master" ]];then
+    docker buildx build --platform linux/amd64,linux/arm64 -t nearprotocol/"${image_name}:${branch}-${commit}" -t nearprotocol/${image_name}:latest --push .
+else 
+    docker buildx build --platform linux/amd64,linux/arm64 -t nearprotocol/"${image_name}:${branch}-${commit}" --push .
+fi
