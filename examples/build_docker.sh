046d82ade13394dea0487e0d6f8c5347dac48cdc
Daniyar Itegulov <ditegulov@gmail.com>
2022-03-24 18:07:21 +1100

    add an arm64 warning

--- examples/build_docker.sh
+++ examples/build_docker.sh
@@ -3,6 +3,10 @@
 # Exit script as soon as a command fails.
 set -ex
 
+# Predefined ANSI escape codes for colors
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
 NAME="$1"
 
 # Switch to current directory (./examples) then out to root for specific examples
@@ -10,7 +14,13 @@ pushd $(dirname ${BASH_SOURCE[0]})
 cd ../
 
 # Pick the correct tag to pull from Docker Hub based on OS architecture
+_warning="
+${YELLOW}WARNING${NC}: You are building smart contracts using ARM64. The resulting artifacts will
+be usable for testing, but won't pass the CI check for inclusion in master due to the
+reproducibility requirements.
+"
 if [[ $(uname -m) == 'arm64' ]]; then
+    echo -e "$_warning"
     TAG="latest-arm64"
 else
     TAG="latest-amd64"
