255f03259e863bd1a396d16846a8029051eacb0c
austinabell <austinabell8@gmail.com>
2022-04-07 20:05:42 -0400

    switch to using max_gas

--- examples/factory-contract/tests/workspaces.rs
+++ examples/factory-contract/tests/workspaces.rs
@@ -12,11 +12,11 @@ async fn test_deploy_status_message(contract_name: &str) -> anyhow::Result<()> {
     let contract = worker.dev_deploy(&std::fs::read(format!("res/{}.wasm", contract_name))?).await?;
 
     let status_id: near_sdk::AccountId = "status".parse().unwrap();
-    let status_amt = U128::from(parse_near!("35 N"));
+    let status_amt = U128::from(parse_near!("20 N"));
     let res = contract
         .call(&worker, "deploy_status_message")
         .args_json((status_id.clone(), status_amt))?
-        .gas(300_000_000_000_000)
+        .max_gas()
         .deposit(parse_near!("50 N"))
         .transact()
         .await?;
@@ -26,7 +26,7 @@ async fn test_deploy_status_message(contract_name: &str) -> anyhow::Result<()> {
     let res = contract
         .call(&worker, "complex_call")
         .args_json((status_id, message))?
-        .gas(300_000_000_000_000)
+        .max_gas()
         .transact()
         .await?;
     assert!(res.is_success());
