b7faccfd2494a82c79aa0d88ccbf8f3e5eeed9d4
Daniyar Itegulov <ditegulov@gmail.com>
2022-06-30 17:28:41 +1000

    extract #[callback_vec] vec type for abi

--- near-sdk-macros/src/core_impl/utils/mod.rs
+++ near-sdk-macros/src/core_impl/utils/mod.rs
@@ -9,6 +9,15 @@ pub(crate) fn path_is_result(path: &Path) -> bool {
         && path.segments.iter().next().unwrap().ident == "Result"
 }
 
+/// Checks whether the given path is literally "Vec".
+/// Note that it won't match a fully qualified name `std::vec::Vec` or a type alias like
+/// `type MyVec = Vec<String>`.
+pub(crate) fn path_is_vec(path: &Path) -> bool {
+    path.leading_colon.is_none()
+        && path.segments.len() == 1
+        && path.segments.iter().next().unwrap().ident == "Vec"
+}
+
 /// Equivalent to `path_is_result` except that it works on `Type` values.
 pub(crate) fn type_is_result(ty: &Type) -> bool {
     match ty {
@@ -31,6 +40,29 @@ pub(crate) fn extract_ok_type(ty: &Type) -> Option<&Type> {
                 _ => None,
             }?;
             // This argument must be a type:
+            match generic_arg {
+                GenericArgument::Type(ty) => Some(ty),
+                _ => None,
+            }
+        }
+        _ => None,
+    }
+}
+
+/// Extracts the inner generic type from a `Vec<_>` type.
+///
+/// For example, given `Vec<String>` this function will return `String`.
+pub(crate) fn extract_vec_type(ty: &Type) -> Option<&Type> {
+    match ty {
+        Type::Path(type_path) if type_path.qself.is_none() && path_is_vec(&type_path.path) => {
+            let type_params = &type_path.path.segments.first()?.arguments;
+            let generic_arg = match type_params {
+                // We are interested in the first (and only) angle-bracketed param:
+                PathArguments::AngleBracketed(params) if params.args.len() == 1 => {
+                    Some(params.args.first()?)
+                }
+                _ => None,
+            }?;
             match generic_arg {
                 GenericArgument::Type(ty) => Some(ty),
                 _ => None,
