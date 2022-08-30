1582333b86d67434b935171149e97af43c96a68f
Miraculous Owonubi <omiraculous@gmail.com>
2022-08-30 22:01:13 +0400

    rename abi dir to adder

--- examples/adder/build.sh
+++ examples/adder/build.sh
@@ -4,5 +4,5 @@ set -e
 cd "$(dirname $0)"
 
 cargo build --target wasm32-unknown-unknown --release
-cp $TARGET/wasm32-unknown-unknown/release/abi.wasm ./res/
+cp $TARGET/wasm32-unknown-unknown/release/adder.wasm ./res/
 #wasm-opt -Oz --output ./res/status_message.wasm ./res/status_message.wasm
