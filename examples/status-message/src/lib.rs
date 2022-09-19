b3d5ebf3a5d483c77316da12d744224c783bc42a
Austin Abell <austinabell8@gmail.com>
2022-09-19 20:07:41 +0100

    oops

--- examples/status-message/src/lib.rs
+++ examples/status-message/src/lib.rs
@@ -1,5 +1,5 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::{env, log, metadata, near_bindgen, AccountId};
+use near_sdk::{env, log, near_bindgen, AccountId};
 use std::collections::HashMap;
 
 #[near_bindgen]
