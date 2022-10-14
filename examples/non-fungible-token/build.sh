375d98fdd7eecad542a1628aa6ca6ccfcc3f334a
Daniyar Itegulov <ditegulov@gmail.com>
2022-10-14 18:47:21 +1100

    make examples use the root target folder

--- examples/non-fungible-token/build.sh
+++ examples/non-fungible-token/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-TARGET="${CARGO_TARGET_DIR:-target}"
+TARGET="${CARGO_TARGET_DIR:-../../target}"
 set -e
-cd "`dirname $0`"
+cd "$(dirname $0)"
 cargo build --all --target wasm32-unknown-unknown --release
 cp $TARGET/wasm32-unknown-unknown/release/approval_receiver.wasm ./res/
 cp $TARGET/wasm32-unknown-unknown/release/non_fungible_token.wasm ./res/
--- examples/test-contract/build.sh
+++ examples/test-contract/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-TARGET="${CARGO_TARGET_DIR:-target}"
+TARGET="${CARGO_TARGET_DIR:-../../target}"
 set -e
-cd "`dirname $0`"
+cd "$(dirname $0)"
 cargo build --target wasm32-unknown-unknown --release
 cp $TARGET/wasm32-unknown-unknown/release/test_contract.wasm ./res/
 #wasm-opt -Oz --output ./res/test_contract.wasm ./res/test_contract.wasm
-
