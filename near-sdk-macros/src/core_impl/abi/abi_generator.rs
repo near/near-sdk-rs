7ee16d949ceeb61e1edae6804b6d15082930db75
Miraculous Owonubi <omiraculous@gmail.com>
2022-10-19 14:09:48 +0400

    return JSON strings from abi chunk generators

--- near-sdk-macros/src/core_impl/abi/abi_generator.rs
+++ near-sdk-macros/src/core_impl/abi/abi_generator.rs
@@ -23,13 +23,15 @@ pub fn generate(i: &ItemImplInfo) -> TokenStream2 {
         #[cfg(not(target_arch = "wasm32"))]
         const _: () = {
             #[no_mangle]
-            pub fn #near_abi_symbol() -> near_sdk::__private::ChunkedAbiEntry {
+            pub fn #near_abi_symbol() -> String {
                 let mut gen = near_sdk::__private::schemars::gen::SchemaGenerator::default();
                 let functions = vec![#(#functions),*];
-                near_sdk::__private::ChunkedAbiEntry::new(
-                    functions,
-                    gen.into_root_schema_for::<String>()
-                )
+                near_sdk::serde_json::to_string(
+                    &near_sdk::__private::ChunkedAbiEntry::new(
+                        functions,
+                        gen.into_root_schema_for::<String>()
+                    )
+                ).unwrap()
             }
         };
     }
