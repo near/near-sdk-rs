f5d3bfd00b4947324e2f38573132ef72b5b76213
Miraculous Owonubi <omiraculous@gmail.com>
2022-10-21 18:15:14 +0400

    std::ffi::c_uchar -> u8

--- near-sdk-macros/src/core_impl/abi/abi_generator.rs
+++ near-sdk-macros/src/core_impl/abi/abi_generator.rs
@@ -23,7 +23,7 @@ pub fn generate(i: &ItemImplInfo) -> TokenStream2 {
         #[cfg(not(target_arch = "wasm32"))]
         const _: () = {
             #[no_mangle]
-            pub extern "C" fn #near_abi_symbol() -> (*const std::ffi::c_uchar, usize) {
+            pub extern "C" fn #near_abi_symbol() -> (*const u8, usize) {
                 let mut gen = near_sdk::__private::schemars::gen::SchemaGenerator::default();
                 let functions = vec![#(#functions),*];
                 let mut data = std::mem::ManuallyDrop::new(
