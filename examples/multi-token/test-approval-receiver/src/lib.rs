f9060cd918b77b0950738a3bc7ff7de8ca863590
A. Romankov <doit.rant@gmail.com>
2022-12-14 13:38:43 -0500

    MT token: added missed param to mt_on_approve

--- examples/multi-token/test-approval-receiver/src/lib.rs
+++ examples/multi-token/test-approval-receiver/src/lib.rs
@@ -1,6 +1,7 @@
 use near_contract_standards::multi_token::{approval::MultiTokenApprovalReceiver, token::TokenId};
 use near_sdk::{
     borsh::{self, BorshDeserialize, BorshSerialize},
+    json_types::U128,
     log, near_bindgen, AccountId, PanicOnDefault, PromiseOrValue,
 };
 
@@ -23,11 +24,18 @@ impl MultiTokenApprovalReceiver for Contract {
     fn mt_on_approve(
         &mut self,
         tokens: Vec<TokenId>,
+        amounts: Vec<U128>,
         owner_id: AccountId,
         approval_ids: Vec<u64>,
         msg: String,
     ) -> PromiseOrValue<String> {
-        log!("Tokens: {:?} Owner: {}, approval_ids: {:?}", tokens, owner_id, approval_ids);
+        log!(
+            "Tokens: {:?} Amounts: {:?} Owner: {}, approval_ids: {:?}",
+            tokens,
+            amounts,
+            owner_id,
+            approval_ids
+        );
         log!(&msg);
 
         PromiseOrValue::Value(ON_MT_TOKEN_APPROVE_MSG.to_string())
