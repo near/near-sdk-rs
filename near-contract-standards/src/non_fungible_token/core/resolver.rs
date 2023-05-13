cdff755bcb79bbf32a211ced6dc9168d915dee39
sakura <zjchen1234@foxmail.com>
2023-05-13 17:55:13 +0800

    fix: rename param `approvals` to `approved_account_ids`

--- near-contract-standards/src/non_fungible_token/core/resolver.rs
+++ near-contract-standards/src/non_fungible_token/core/resolver.rs
@@ -26,7 +26,7 @@ pub trait NonFungibleTokenResolver {
     /// * `previous_owner_id`: the owner prior to the call to `nft_transfer_call`
     /// * `receiver_id`: the `receiver_id` argument given to `nft_transfer_call`
     /// * `token_id`: the `token_id` argument given to `ft_transfer_call`
-    /// * `approvals`: if using Approval Management, contract MUST provide
+    /// * `approved_account_ids`: if using Approval Management, contract MUST provide
     ///   set of original approved accounts in this argument, and restore these
     ///   approved accounts in case of revert.
     ///
@@ -36,6 +36,6 @@ pub trait NonFungibleTokenResolver {
         previous_owner_id: AccountId,
         receiver_id: AccountId,
         token_id: TokenId,
-        approvals: Option<HashMap<AccountId, u64>>,
+        approved_account_ids: Option<HashMap<AccountId, u64>>,
     ) -> bool;
 }
