dd6f96a294818775a61bb897d5c87ebc10897536
Vlad Frolov <frolvlad@gmail.com>
2023-05-23 22:57:01 +0200

    refactor: Use shorter and explicit methods

--- near-contract-standards/src/non_fungible_token/core/core_impl.rs
+++ near-contract-standards/src/non_fungible_token/core/core_impl.rs
@@ -209,13 +209,13 @@ impl NonFungibleToken {
         let approved_account_ids = self
             .approvals_by_id
             .as_mut()
-            .and_then(|by_id| by_id.remove(token_id).or_else(|| Some(HashMap::new())));
+            .map(|by_id| by_id.remove(token_id).unwrap_or_default());
 
         // check if authorized
         let sender_id = if sender_id != &owner_id {
-            // if approval extension is NOT being used, or if token has no approved accounts
+            // Panic if approval extension is NOT being used
             let app_acc_ids =
-                approved_account_ids.as_ref().unwrap_or_else(|| env::panic_str("Unauthorized"));
+                approved_account_ids.as_ref().unwrap_or_else(|| env::panic_str("Approval extension is disabled"));
 
             // Approval extension is being used; get approval_id for sender.
             let actual_approval_id = app_acc_ids.get(sender_id);
