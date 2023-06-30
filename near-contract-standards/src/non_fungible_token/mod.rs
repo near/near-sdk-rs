debe8b87ee60a52c66714ec881f5e8b6ca45337d
Roman Useinov <roman.useinov@gmail.com>
2023-06-30 16:44:04 +0200

    [Enhancement] NFT: remove declarative macros.

--- near-contract-standards/src/non_fungible_token/mod.rs
+++ near-contract-standards/src/non_fungible_token/mod.rs
@@ -6,9 +6,6 @@ pub mod core;
 /// Trait for the [NFT enumeration standard](https://nomicon.io/Standards/NonFungibleToken/Enumeration.html).
 /// This provides useful view-only methods returning token supply, tokens by owner, etc.
 pub mod enumeration;
-/// Macros typically used by a contract wanting to take advantage of the non-fungible
-/// token NEAR contract standard approach.
-mod macros;
 /// Metadata traits and implementation according to the [NFT enumeration standard](https://nomicon.io/Standards/NonFungibleToken/Metadata.html).
 /// This covers both the contract metadata and the individual token metadata.
 pub mod metadata;
@@ -21,6 +18,5 @@ mod utils;
 pub use utils::*;
 
 pub use self::core::NonFungibleToken;
-pub use macros::*;
 
 pub mod events;
