bf70e418ebb766c157aaf83d1e22ceeed25d2574
Tomasz Kurcz <uint@users.noreply.github.com>
2023-07-03 19:12:53 +0200

    compare_sizes: report generation

--- scripts/compare_sizes.py
+++ scripts/compare_sizes.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+
+# Requires:
+# `pip install GitPython`
 import os
 import tempfile
 import glob
@@ -44,6 +47,33 @@ class ProjectInstance:
 cur_branch = ProjectInstance(project_root)
 repo = Repo(project_root)
 
+def report(master, this_branch):
+    def diff(old, new):
+        diff = (new - old)/old
+
+        res = "{0:+.0%}".format(diff)
+        if diff < -.1:
+            res = f"<div class=\"good\">{res}</div>"
+        elif diff > .1:
+            res = f"<div class=\"bad\">{res}</div>"
+
+        return res
+
+    header = """<style>.good{color:Green;}</style>
+<style>.bad{color:Red;}</style>
+
+# Contract size report
+
+Sizes are given in bytes.
+
+| contract | master | this branch | difference |
+| - | - | - | - |"""
+
+    combined = [(name,master,branch,diff(master, branch)) for name, master, branch in common_entries(master, this_branch)]
+    rows = [f"| {name} | {old} | {new} | {diff} |" for name, old, new, diff in combined]
+
+    return "\n".join([header, *rows])
+
 try:
     with tempfile.TemporaryDirectory() as tempdir:
         repo.git.worktree("add", tempdir, "master")
@@ -51,7 +81,6 @@ try:
 
         master_sizes = master.sizes()
         cur_sizes = cur_branch.sizes()
-        print(master_sizes)
-        print(cur_sizes)
+        print(report(master_sizes, cur_sizes))
 finally:
     repo.git.worktree("prune")
