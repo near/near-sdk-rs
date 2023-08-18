6b2aef4e612388fb72201643cfab02a7b532bef2
Tomasz Kurcz <uint@users.noreply.github.com>
2023-08-18 20:31:46 +0200

    lint

--- near-sdk-macros/src/core_impl/abi/abi_generator.rs
+++ near-sdk-macros/src/core_impl/abi/abi_generator.rs
@@ -204,7 +204,7 @@ impl ImplItemMethodInfo {
         match &self.attr_signature_info.returns.kind {
             Default => quote! { ::std::option::Option::None },
             General(ty) => self.abi_result_tokens_with_return_value(ty),
-            HandlesResult { ty } => {
+            HandlesResult(ty) => {
                 // extract the `Ok` type from the result
                 let ty = parse_quote! { <#ty as near_sdk::__private::ResultTypeExt>::Okay };
                 self.abi_result_tokens_with_return_value(&ty)
