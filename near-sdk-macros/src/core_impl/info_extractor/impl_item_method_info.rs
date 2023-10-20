34953ee14b88304a2945585d26013838a42e1154
iho <iho@users.noreply.github.com>
2023-10-20 16:45:20 +0300

    chore: fix test

--- near-sdk-macros/src/core_impl/info_extractor/impl_item_method_info.rs
+++ near-sdk-macros/src/core_impl/info_extractor/impl_item_method_info.rs
@@ -71,7 +71,7 @@ mod tests {
             pub fn method(&self) -> &'static str { }
         };
         let actual = ImplItemMethodInfo::new(&mut method, false, impl_type).map(|_| ()).unwrap_err();
-        let expected = "Function marked with #[handle_result] should return Result<T, E> (where E implements FunctionError).";
+        let expected = "Function marked with #[handle_result] should return Result<T, E> (where E implements FunctionError). If you're trying to use a type alias for `Result`, try `#[handle_result(aliased)]`.";
         assert_eq!(expected, actual.to_string());
     }
 
