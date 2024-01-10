13430592a7be246dd5a29439791f4081e0107ff3
dj8yf0μl <26653921+dj8yfo@users.noreply.github.com>
2024-01-10 23:39:12 +0200

    ci: fix new 1.75 warnings (#1128)

--- near-contract-standards/src/non_fungible_token/mod.rs
+++ near-contract-standards/src/non_fungible_token/mod.rs
@@ -20,8 +20,6 @@ pub use self::token::{Token, TokenId};
 mod utils;
 pub use utils::*;
 
-pub use macros::*;
-
 pub use self::approval::NonFungibleTokenApproval;
 pub use self::core::NonFungibleToken;
 pub use self::core::NonFungibleTokenResolver;
