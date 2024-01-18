e04b2762db72afdd767ba8045150038b62950cfe
Saketh Are <saketh.are@gmail.com>
2024-01-17 22:32:21 -0500

    clean up

--- examples/mpc-contract/create.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-env NEAR_ENV=localnet near create-account "$1.node0" --keyPath ~/.near/localnet/node0/validator_key.json --masterAccount node0
--- examples/mpc-contract/deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-env NEAR_ENV=localnet near deploy "$1.node0" res/mpc_contract.wasm
