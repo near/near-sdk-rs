623032888e4093650c47206b87b7d9ea4693df13
Saketh Are <saketh.are@gmail.com>
2024-02-15 17:18:22 -0500

    remove need for separate cleanup callback

--- examples/mpc-contract/src/lib.rs
+++ examples/mpc-contract/src/lib.rs
@@ -1,8 +1,8 @@
 use near_sdk::borsh::{BorshDeserialize, BorshSerialize};
 use near_sdk::collections::TreeMap;
 use near_sdk::{
-    env, log, near_bindgen, require, serde_json, AccountId, BorshStorageKey, CryptoHash, Gas,
-    GasWeight, NearToken, PromiseError,
+    env, log, near_bindgen, serde_json, AccountId, BorshStorageKey, CryptoHash, Gas, GasWeight,
+    PromiseError,
 };
 
 #[derive(BorshSerialize, BorshStorageKey)]
@@ -12,6 +12,7 @@ struct RecordsKey;
 #[derive(BorshDeserialize, BorshSerialize)]
 #[borsh(crate = "near_sdk::borsh")]
 pub struct SignatureRequest {
+    data_id: CryptoHash,
     account_id: AccountId,
     payload: String,
 }
@@ -20,12 +21,13 @@ pub struct SignatureRequest {
 #[derive(BorshDeserialize, BorshSerialize)]
 #[borsh(crate = "near_sdk::borsh")]
 pub struct MpcContract {
-    requests: TreeMap<CryptoHash, SignatureRequest>,
+    requests: TreeMap<u64, SignatureRequest>,
+    next_available_request_index: u64,
 }
 
 impl Default for MpcContract {
     fn default() -> Self {
-        Self { requests: TreeMap::new(RecordsKey) }
+        Self { requests: TreeMap::new(RecordsKey), next_available_request_index: 0u64 }
     }
 }
 
@@ -35,16 +37,16 @@ const DATA_ID_REGISTER: u64 = 0;
 // Prepaid gas for a `sign_on_finish` call
 const SIGN_ON_FINISH_CALL_GAS: Gas = Gas::from_tgas(5);
 
-// Prepaid gas for a `sign_on_finish` call
-const REMOVE_REQUEST_CALL_GAS: Gas = Gas::from_tgas(5);
-
 #[near_bindgen]
 impl MpcContract {
     /// User-facing API: accepts payload and returns signature
     pub fn sign(&mut self, payload: String) {
+        let index = self.next_available_request_index;
+        self.next_available_request_index += 1;
+
         let promise = env::promise_yield_create(
             "sign_on_finish",
-            &[],
+            &serde_json::to_vec(&(index,)).unwrap(),
             SIGN_ON_FINISH_CALL_GAS,
             GasWeight(0),
             DATA_ID_REGISTER,
@@ -53,17 +55,9 @@ impl MpcContract {
         // Record the pending request
         let data_id: CryptoHash =
             env::read_register(DATA_ID_REGISTER).expect("").try_into().expect("");
-        self.requests
-            .insert(&data_id, &SignatureRequest { account_id: env::signer_account_id(), payload });
-
-        // Schedule clean-up of self.requests after the yield execution completes
-        env::promise_then(
-            promise,
-            env::current_account_id(),
-            "remove_request",
-            &serde_json::to_vec(&(data_id,)).unwrap(),
-            NearToken::from_near(0),
-            REMOVE_REQUEST_CALL_GAS,
+        self.requests.insert(
+            &index,
+            &SignatureRequest { data_id, account_id: env::signer_account_id(), payload },
         );
 
         env::promise_return(promise);
@@ -75,8 +69,6 @@ impl MpcContract {
         hex::decode_to_slice(data_id, &mut data_id_buf).expect("");
         let data_id = data_id_buf;
 
-        require!(self.requests.contains_key(&data_id));
-
         // check that caller is allowed to respond, signature is valid, etc.
         // ...
 
@@ -84,27 +76,27 @@ impl MpcContract {
         env::promise_yield_resume(&data_id, &serde_json::to_vec(&signature).unwrap());
     }
 
-    /// Callback receiving the externally submitted data
+    /// Callback receiving the externally submitted data (or a PromiseError)
     pub fn sign_on_finish(
+        &mut self,
+        request_index: u64,
         #[callback_result] signature: Result<String, PromiseError>,
     ) -> Option<String> {
+        // clean up local state
+        self.requests.remove(&request_index);
+
         match signature {
-            Ok(signature) => Some(signature + " post"),
+            Ok(signature) => Some("signature received: ".to_owned() + &signature),
             Err(_) => Some("signature request timed out".to_string()),
         }
     }
 
-    /// Callback used to clean up the local state of the contract
-    pub fn remove_request(&mut self, data_id: CryptoHash) {
-        self.requests.remove(&data_id);
-    }
-
     /// Helper for local testing; prints all pending requests
     pub fn log_pending_requests(&self) {
-        for (data_id, request) in self.requests.iter() {
+        for (_, request) in self.requests.iter() {
             log!(
                 "{}: account_id={} payload={}",
-                hex::encode(data_id),
+                hex::encode(request.data_id),
                 request.account_id,
                 request.payload
             );
