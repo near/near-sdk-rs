2b19c0a862a9eaadc381ab9529a13a5e1bb462bc
Saketh Are <saketh.are@gmail.com>
2024-02-15 18:04:02 -0500

    add back promise_then example

--- examples/mpc-contract/src/lib.rs
+++ examples/mpc-contract/src/lib.rs
@@ -2,7 +2,7 @@ use near_sdk::borsh::{BorshDeserialize, BorshSerialize};
 use near_sdk::collections::TreeMap;
 use near_sdk::{
     env, log, near_bindgen, serde_json, AccountId, BorshStorageKey, CryptoHash, Gas, GasWeight,
-    PromiseError,
+    NearToken, PromiseError,
 };
 
 #[derive(BorshSerialize, BorshStorageKey)]
@@ -14,7 +14,7 @@ struct RecordsKey;
 pub struct SignatureRequest {
     data_id: CryptoHash,
     account_id: AccountId,
-    payload: String,
+    message: String,
 }
 
 #[near_bindgen]
@@ -37,14 +37,17 @@ const DATA_ID_REGISTER: u64 = 0;
 // Prepaid gas for a `sign_on_finish` call
 const SIGN_ON_FINISH_CALL_GAS: Gas = Gas::from_tgas(5);
 
+// Prepaid gas for a `do_something` call
+const CHAINED_CALL_GAS: Gas = Gas::from_tgas(5);
+
 #[near_bindgen]
 impl MpcContract {
-    /// User-facing API: accepts payload and returns signature
-    pub fn sign(&mut self, payload: String) {
+    /// User-facing API: accepts some message and returns a signature
+    pub fn sign(&mut self, message_to_be_signed: String) {
         let index = self.next_available_request_index;
         self.next_available_request_index += 1;
 
-        let promise = env::promise_yield_create(
+        let yield_promise = env::promise_yield_create(
             "sign_on_finish",
             &serde_json::to_vec(&(index,)).unwrap(),
             SIGN_ON_FINISH_CALL_GAS,
@@ -57,12 +60,28 @@ impl MpcContract {
             env::read_register(DATA_ID_REGISTER).expect("").try_into().expect("");
         self.requests.insert(
             &index,
-            &SignatureRequest { data_id, account_id: env::signer_account_id(), payload },
+            &SignatureRequest {
+                data_id,
+                account_id: env::signer_account_id(),
+                message: message_to_be_signed,
+            },
+        );
+
+        // The yield promise is composable with the usual promise API features. We can choose to
+        // chain another function call and it will receive the output of the `sign_on_finish`
+        // callback. Note that this chained promise can be a cross-contract call.
+        env::promise_then(
+            yield_promise,
+            env::current_account_id(),
+            "do_something",
+            &[],
+            NearToken::from_near(0),
+            CHAINED_CALL_GAS,
         );
 
         // The return value for this function call will be the value
         // returned by the `sign_on_finish` callback.
-        env::promise_return(promise);
+        env::promise_return(yield_promise);
     }
 
     /// Called by MPC participants to submit a signature
@@ -83,14 +102,18 @@ impl MpcContract {
         &mut self,
         request_index: u64,
         #[callback_result] signature: Result<String, PromiseError>,
-    ) -> Option<String> {
+    ) -> String {
         // Clean up the local state
         self.requests.remove(&request_index);
 
         match signature {
-            Ok(signature) => Some("signature received: ".to_owned() + &signature),
-            Err(_) => Some("signature request timed out".to_string()),
+            Ok(signature) => "signature received: ".to_owned() + &signature,
+            Err(_) => "signature request timed out".to_string(),
         }
+    }
+
+    pub fn do_something(#[callback_unwrap] signature_result: String) {
+        log!("fn do_something invoked with result '{}'", &signature_result);
     }
 
     /// Helper for local testing; prints all pending requests
@@ -100,7 +123,7 @@ impl MpcContract {
                 "{}: account_id={} payload={}",
                 hex::encode(request.data_id),
                 request.account_id,
-                request.payload
+                request.message
             );
         }
     }
