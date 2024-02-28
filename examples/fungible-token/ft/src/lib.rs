fa96e563118a4f7aaa67f550a4c8142cbff066b4
polyprogrammist <vad.e.volodin@gmail.com>
2024-02-28 09:44:49 -0500

    move borshstoragekey out in example

--- examples/fungible-token/ft/src/lib.rs
+++ examples/fungible-token/ft/src/lib.rs
@@ -27,8 +27,11 @@ use near_contract_standards::storage_management::{
 use near_sdk::collections::LazyOption;
 use near_sdk::json_types::U128;
 use near_sdk::{
-    env, log, near, require, AccountId, BorshStorageKey, NearToken, PanicOnDefault, PromiseOrValue,
+    env, log, near, require, AccountId, BorshStorageKey, NearStorageKey, NearToken, PanicOnDefault,
+    PromiseOrValue,
 };
+use near_sdk::borsh;
+use near_sdk::borsh::BorshSerialize;
 
 #[derive(PanicOnDefault)]
 #[near(contract_state)]
@@ -39,12 +42,29 @@ pub struct Contract {
 
 const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
 
-#[derive(BorshStorageKey)]
-#[near]
+// #[derive(NearStorageKey)]
+#[derive(BorshSerialize)]
+#[borsh(crate = "near_sdk::borsh")]
 enum StorageKey {
     FungibleToken,
     Metadata,
 }
+
+const _: () = {
+    #[allow(non_camel_case_types)]
+    type StorageKey__NEAR_SCHEMA_PROXY = StorageKey;
+    {
+        #[derive(::near_sdk::borsh::BorshSerialize, ::near_sdk::BorshStorageKey)]
+        #[borsh(crate = "near_sdk::borsh")]
+        enum StorageKey {
+            FungibleToken,
+            Metadata,
+        }
+        #[automatically_derived]
+        impl ::near_sdk::__private::BorshIntoStorageKey for StorageKey__NEAR_SCHEMA_PROXY {
+        }
+    };
+};
 
 #[near(contract_state)]
 impl Contract {
