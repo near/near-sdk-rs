e0921032fa104a9080433fab76dbb7cb2cc0f35e
polyprogrammist <vad.e.volodin@gmail.com>
2024-03-12 11:13:40 -0500

    remove comments

--- near-sdk/compilation_tests/contract_metadata.rs
+++ near-sdk/compilation_tests/contract_metadata.rs
@@ -1,9 +1,5 @@
-use near_sdk::near as near;
+use near_sdk::near;
 
-// #[derive(near_sdk::NearSchema)]
-// #[derive(near_sdk::borsh::BorshSerialize, near_sdk::borsh::BorshDeserialize)]
-// #[borsh(crate = "near_sdk::borsh")]
-// #[abi(borsh)]
 #[near(contract_state, contract_metadata(
     version = "39f2d2646f2f60e18ab53337501370dc02a5661c",
     link = "https://github.com/near-examples/nft-tutorial",
