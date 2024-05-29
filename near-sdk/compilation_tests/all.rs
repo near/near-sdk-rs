17df48cdbec09c0f6300276f4365aebefc9b4d4e
polyprogrammist <vad.e.volodin@gmail.com>
2024-05-29 13:45:43 +0800

    add compilation failed test for incorrect result

--- near-sdk/compilation_tests/all.rs
+++ near-sdk/compilation_tests/all.rs
@@ -37,4 +37,5 @@ fn compilation_tests() {
     t.compile_fail("compilation_tests/contract_metadata_fn_name.rs");
     t.pass("compilation_tests/contract_metadata_bindgen.rs");
     t.pass("compilation_tests/types.rs");
+    t.compile_fail("compilation_tests/error_handling_incorrect_result.rs");
 }
