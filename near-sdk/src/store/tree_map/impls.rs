f5e9db678590bef18d101bb49020df0dc44ec527
polyprogrammist <vad.e.volodin@gmail.com>
2024-05-31 12:16:43 +0800

    fix compilation test

--- near-sdk/src/store/tree_map/impls.rs
+++ near-sdk/src/store/tree_map/impls.rs
@@ -2,8 +2,8 @@ use std::borrow::Borrow;
 
 use borsh::{BorshDeserialize, BorshSerialize};
 
-use crate::env;
-use crate::store::{key::ToKey, TreeMap, ERR_NOT_EXIST};
+use crate::{env, standard_errors};
+use crate::store::{key::ToKey, TreeMap};
 
 impl<K, V, H> Extend<(K, V)> for TreeMap<K, V, H>
 where
