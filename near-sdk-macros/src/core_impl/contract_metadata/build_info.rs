ce5c116f9362eba6b9a3e3394c98261faaf94cc6
dj8yf0μl <feepdake27@gmail.com>
2024-06-10 16:32:33 +0300

    review: addresses https://github.com/near/near-sdk-rs/pull/1178#discussion_r1628149583, part 2

--- near-sdk-macros/src/core_impl/contract_metadata/build_info.rs
+++ near-sdk-macros/src/core_impl/contract_metadata/build_info.rs
@@ -22,29 +22,22 @@ const ERR_UNSET_OR_EMPTY_SOURCE_SNAPSHOT: &str = "`CARGO_NEAR_SOURCE_CODE_GIT_UR
 impl BuildInfo {
     pub(super) fn from_env() -> Result<Self, String> {
         let build_environment = std::env::var("CARGO_NEAR_BUILD_ENVIRONMENT")
-            .map_err(|_| ERR_EMPTY_BUILD_ENVIRONMENT.to_string())?;
-        if build_environment.is_empty() {
-            return Err(ERR_EMPTY_BUILD_ENVIRONMENT.to_string());
-        }
+            .ok()
+            .filter(|build_environment| !build_environment.is_empty())
+            .ok_or(ERR_EMPTY_BUILD_ENVIRONMENT.to_string())?;
 
-        let build_command = {
-            let build_command = std::env::var("CARGO_NEAR_BUILD_COMMAND")
-                .map_err(|_| ERR_EMPTY_BUILD_COMMAND.to_string())?;
-            if build_command.is_empty() {
-                return Err(ERR_EMPTY_BUILD_COMMAND.to_string());
-            }
-            let build_command =
-                build_command.split_whitespace().map(|st| st.to_string()).collect::<Vec<_>>();
-            if build_command.is_empty() {
-                return Err(ERR_EMPTY_BUILD_COMMAND.to_string());
-            }
-            build_command
-        };
+        let build_command = std::env::var("CARGO_NEAR_BUILD_COMMAND")
+            .ok()
+            .filter(|build_command| !build_command.is_empty())
+            .map(|build_command| {
+                build_command.split_whitespace().map(|st| st.to_string()).collect::<Vec<_>>()
+            })
+            .filter(|build_command| !build_command.is_empty())
+            .ok_or(ERR_EMPTY_BUILD_COMMAND.to_string())?;
         let source_code_snapshot = std::env::var("CARGO_NEAR_SOURCE_CODE_SNAPSHOT")
-            .map_err(|_| ERR_UNSET_OR_EMPTY_SOURCE_SNAPSHOT.to_string())?;
-        if source_code_snapshot.is_empty() {
-            return Err(ERR_UNSET_OR_EMPTY_SOURCE_SNAPSHOT.to_string());
-        }
+            .ok()
+            .filter(|source_code_snapshot| !source_code_snapshot.is_empty())
+            .ok_or(ERR_UNSET_OR_EMPTY_SOURCE_SNAPSHOT.to_string())?;
         let contract_path = std::env::var("CARGO_NEAR_CONTRACT_PATH")
             .map_err(|_| ERR_UNSET_CONTRACT_PATH.to_string())?;
 
