1ca16a408b08920aa06821ba35152f9a0aae0761
dj8yf0μl <feepdake27@gmail.com>
2024-06-27 19:10:16 +0300

    ci: remove too much movement around `compilation_tests/schema_derive_invalids.rs`

--- near-sdk/compilation_tests/all.rs
+++ near-sdk/compilation_tests/all.rs
@@ -21,9 +21,9 @@ fn compilation_tests() {
     t.pass("compilation_tests/function_error.rs");
     t.pass("compilation_tests/enum_near_bindgen.rs");
     t.pass("compilation_tests/schema_derive.rs");
-    if rustversion::cfg!(since(1.72)) {
-        // The compilation error output has slightly changed in 1.72, so we
-        // snapshoted this new version
+    if rustversion::cfg!(since(1.78)) && std::env::consts::OS == "linux" {
+        // The compilation error output has slightly changed in 1.7x and between platforms,
+        // so we snapshoted this single version
         t.compile_fail("compilation_tests/schema_derive_invalids.rs");
     }
     t.compile_fail("compilation_tests/generic_function.rs");
