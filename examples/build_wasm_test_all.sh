78f4e18c44b2d19d7094ffcf9879e5cc0f3bf1d0
jrmncos <costillagermanalfredo@gmail.com>
2024-08-05 01:27:28 -0300

    minor fix

--- examples/build_wasm_test_all.sh
+++ examples/build_wasm_test_all.sh
@@ -11,15 +11,15 @@ for dir in "$BASE_DIR"/*; do
 
         echo "Building $dir"
 
-        cargo +CARGO_VERSION wasmcov build -- -Z build-std --all --target wasm32-unknown-unknown --release
+        cargo +${CARGO_VERSION} wasmcov build -- -Z build-std --all --target wasm32-unknown-unknown --release
 
         echo "Testing $dir"
 
-        cargo +CARGO_VERSION test -- --workspace
+        cargo +${CARGO_VERSION} test -- --workspace
 
         echo "Generating report"
 
-        cargo +CARGO_VERSION wasmcov report
+        cargo +${CARGO_VERSION} wasmcov report
 
         ls -lha
 
