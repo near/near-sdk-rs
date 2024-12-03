d716b27518657000907aeec2465e2b3a92066cd3
aiqubit <aiqubit@hotmail.com>
2024-12-03 22:09:17 +0800

    fix: Fix the input parameter of example nft_mint method as Option<TokenMetadata>

--- examples/non-fungible-token/nft/src/lib.rs
+++ examples/non-fungible-token/nft/src/lib.rs
@@ -100,10 +100,10 @@ impl Contract {
         &mut self,
         token_id: TokenId,
         token_owner_id: AccountId,
-        token_metadata: TokenMetadata,
+        token_metadata: Option<TokenMetadata>,
     ) -> Token {
         assert_eq!(env::predecessor_account_id(), self.tokens.owner_id, "Unauthorized");
-        self.tokens.internal_mint(token_id, token_owner_id, Some(token_metadata))
+        self.tokens.internal_mint(token_id, token_owner_id, token_metadata)
     }
 }
 
