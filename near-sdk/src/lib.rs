4f715a450eadb6b79e8c6b861a56bc6dfc7f283d
akorchyn <artur.yurii.korchynskyi@gmail.com>
2025-02-04 12:06:51 +0200

    review 2

--- near-sdk/src/lib.rs
+++ near-sdk/src/lib.rs
@@ -245,13 +245,13 @@ extern crate quickcheck;
 /// Also, the source metadata will be stored as a constant, `CONTRACT_SOURCE_METADATA`, in the contract code.
 ///
 /// ## Examples
+///
 /// ```rust
 /// use near_sdk::near;
 ///
 /// #[near(contract_metadata(
 ///     version = "39f2d2646f2f60e18ab53337501370dc02a5661c",
 ///     link = "https://github.com/near-examples/nft-tutorial",
-///     standard(standard = "nep330", version = "1.1.0"),
 ///     standard(standard = "nep171", version = "1.0.0"),
 ///     standard(standard = "nep177", version = "2.0.0"),
 /// ))]
@@ -316,7 +316,7 @@ pub use near_sdk_macros::ext_contract;
 /// ```
 pub use near_sdk_macros::near_bindgen;
 
-/// `BorshStorageKey` generates implementation for [BorshIntoStorageKey](crate::private::BorshIntoStorageKey) trait.
+/// `BorshStorageKey` generates implementation for [BorshIntoStorageKey](crate::__private::BorshIntoStorageKey) trait.
 /// It allows the type to be passed as a unique prefix for persistent collections.
 /// The type should also implement or derive [BorshSerialize](borsh::BorshSerialize) trait.
 ///
