6fb2a6a240c93c5305bb2108b751025cc4879801
Kene Akub <kengoon19@gmail.com>
2025-04-08 04:00:48 +0000

    fix: update build script to use cargo for building wasm

--- examples/build_all.sh
+++ examples/build_all.sh
@@ -7,7 +7,8 @@ pushd $(dirname ${BASH_SOURCE[0]})
 
 for d in "status-message"  $(ls -d */ | grep -v -e "status-message\/$"); do
     echo building $d;
-    (cd "$d"; ./build.sh);
+    (cd "$d"; cargo near build non-reproducible-wasm; cp target/wasm32-unknown-unknown/release/*.wasm res/;);
+    break;
 done
 
 popd
