be05437a29fe402b61fd0bb1f6138ef75011d771
Kene Akub <kengoon19@gmail.com>
2025-04-14 21:29:36 +0000

    remove use of build.sh

--- examples/build_all.sh
+++ examples/build_all.sh
@@ -6,8 +6,12 @@ echo $(rustc --version)
 pushd $(dirname ${BASH_SOURCE[0]})
 
 for d in "status-message"  $(ls -d */ | grep -v -e "status-message\/$"); do
-    echo building $d;
-    (cd "$d"; cargo near build non-reproducible-wasm; cp target/wasm32-unknown-unknown/release/*.wasm res/;);
+    for directory in $(find $d -type d); do
+        if [ -d "$directory/src" ]; then
+            echo building $d;
+            (cd "$d"; cargo near build non-reproducible-wasm; cp target/wasm32-unknown-unknown/release/*.wasm res/;);
+        fi
+    done
 done
 
 popd
