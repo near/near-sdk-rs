85a1f777f91b5e943e43dce55116911916be6006
r-near <163825889+r-near@users.noreply.github.com>
2025-04-18 09:47:41 -0700

    feat: Added BLS12-381 curve operations support in near-sys and exposed in near_sdk::env (#1346)

--- near-sdk/src/environment/mock/mocked_blockchain.rs
+++ near-sdk/src/environment/mock/mocked_blockchain.rs
@@ -636,6 +636,70 @@ mod mock_chain {
     extern "C-unwind" fn alt_bn128_pairing_check(value_len: u64, value_ptr: u64) -> u64 {
         with_mock_interface(|b| b.alt_bn128_pairing_check(value_len, value_ptr))
     }
+
+    // ###########
+    // BLS12-381 #
+    // ###########
+    #[no_mangle]
+    extern "C-unwind" fn bls12381_p1_sum(value_len: u64, value_ptr: u64, register_id: u64) -> u64 {
+        with_mock_interface(|b| b.bls12381_p1_sum(value_len, value_ptr, register_id))
+    }
+    #[no_mangle]
+    extern "C-unwind" fn bls12381_p2_sum(value_len: u64, value_ptr: u64, register_id: u64) -> u64 {
+        with_mock_interface(|b| b.bls12381_p2_sum(value_len, value_ptr, register_id))
+    }
+    #[no_mangle]
+    extern "C-unwind" fn bls12381_g1_multiexp(
+        value_len: u64,
+        value_ptr: u64,
+        register_id: u64,
+    ) -> u64 {
+        with_mock_interface(|b| b.bls12381_g1_multiexp(value_len, value_ptr, register_id))
+    }
+    #[no_mangle]
+    extern "C-unwind" fn bls12381_g2_multiexp(
+        value_len: u64,
+        value_ptr: u64,
+        register_id: u64,
+    ) -> u64 {
+        with_mock_interface(|b| b.bls12381_g2_multiexp(value_len, value_ptr, register_id))
+    }
+    #[no_mangle]
+    extern "C-unwind" fn bls12381_map_fp_to_g1(
+        value_len: u64,
+        value_ptr: u64,
+        register_id: u64,
+    ) -> u64 {
+        with_mock_interface(|b| b.bls12381_map_fp_to_g1(value_len, value_ptr, register_id))
+    }
+    #[no_mangle]
+    extern "C-unwind" fn bls12381_map_fp2_to_g2(
+        value_len: u64,
+        value_ptr: u64,
+        register_id: u64,
+    ) -> u64 {
+        with_mock_interface(|b| b.bls12381_map_fp2_to_g2(value_len, value_ptr, register_id))
+    }
+    #[no_mangle]
+    extern "C-unwind" fn bls12381_pairing_check(value_len: u64, value_ptr: u64) -> u64 {
+        with_mock_interface(|b| b.bls12381_pairing_check(value_len, value_ptr))
+    }
+    #[no_mangle]
+    extern "C-unwind" fn bls12381_p1_decompress(
+        value_len: u64,
+        value_ptr: u64,
+        register_id: u64,
+    ) -> u64 {
+        with_mock_interface(|b| b.bls12381_p1_decompress(value_len, value_ptr, register_id))
+    }
+    #[no_mangle]
+    extern "C-unwind" fn bls12381_p2_decompress(
+        value_len: u64,
+        value_ptr: u64,
+        register_id: u64,
+    ) -> u64 {
+        with_mock_interface(|b| b.bls12381_p2_decompress(value_len, value_ptr, register_id))
+    }
 }
 
 #[cfg(test)]
