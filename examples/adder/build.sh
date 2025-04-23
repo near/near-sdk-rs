b9081ef1a6399a80b3cb4bdc5ce5c5b05d29005a
Kene Akub <kengoon19@gmail.com>
2025-04-23 13:52:52 +0000

    Delete all build.sh, res and build_docker.sh files

--- examples/adder/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TARGET="${CARGO_TARGET_DIR:-../../target}"
-set -e
-cd "$(dirname $0)"
-
-cargo build --target wasm32-unknown-unknown --release
-cp $TARGET/wasm32-unknown-unknown/release/adder.wasm ./res/
-#wasm-opt -Oz --output ./res/status_message.wasm ./res/status_message.wasm
