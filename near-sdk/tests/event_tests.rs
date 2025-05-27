e29a22488843fe8075354b4ff101d1704eddc4f2
BonelessImpl <bonelessimpl@gmail.com>
2025-05-27 17:26:20 +0400

    Fix missing variable

--- near-sdk/tests/event_tests.rs
+++ near-sdk/tests/event_tests.rs
@@ -80,6 +80,9 @@ fn test_json_emit() {
         assert_eq!(log1.get("data").unwrap(), "string");
     }
     {
+    {
+        let log2: serde_json::Value =
+            serde_json::from_str(&logs[2].strip_prefix("EVENT_JSON:").unwrap()).unwrap();
         assert_eq!(log2.as_object().unwrap().len(), 3);
         assert_eq!(log2.get("standard").unwrap(), "test_standard");
         assert_eq!(log2.get("version").unwrap(), "3.0.0");
