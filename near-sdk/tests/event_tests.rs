d08dea5e5f0e99207820f84df1f0fc849c2e6bac
BonelessImpl <bonelessimpl@gmail.com>
2025-05-27 17:23:46 +0400

    Fix event logging tests

--- near-sdk/tests/event_tests.rs
+++ near-sdk/tests/event_tests.rs
@@ -53,28 +53,64 @@ fn test_json_emit() {
 
     let logs = get_logs();
 
-    assert_eq!(
-        logs[0],
-        r#"EVENT_JSON:{"standard":"test_standard","version":"1.0.0","event":"swap","data":{"token_in":"wrap.near","token_out":"test.near","amount_in":100,"amount_out":200,"test":"tst"}}"#
-    );
-    assert_eq!(
-        logs[1],
-        r#"EVENT_JSON:{"standard":"test_standard","version":"2.0.0","event":"string_event","data":"string"}"#
-    );
-    assert_eq!(
-        logs[2],
-        r#"EVENT_JSON:{"standard":"test_standard","version":"3.0.0","event":"empty_event"}"#
-    );
-    assert_eq!(
-        logs[3],
-        r#"EVENT_JSON:{"standard":"test_standard","version":"4.0.0","event":"lifetime_test_a","data":"lifetime"}"#
-    );
-    assert_eq!(
-        logs[4],
-        r#"EVENT_JSON:{"standard":"test_standard","version":"5.0.0","event":"lifetime_test_b","data":"lifetime_b"}"#
-    );
-    assert_eq!(
-        logs[5],
-        r#"EVENT_JSON:{"standard":"another_standard","version":"1.0.0","event":"test"}"#
-    );
+    {
+        let log0: serde_json::Value =
+            serde_json::from_str(&logs[0].strip_prefix("EVENT_JSON:").unwrap()).unwrap();
+
+        assert_eq!(log0.as_object().unwrap().len(), 4);
+        assert_eq!(log0.get("standard").unwrap(), "test_standard");
+        assert_eq!(log0.get("version").unwrap(), "1.0.0");
+        assert_eq!(log0.get("event").unwrap(), "swap");
+
+        let data0 = log0.get("data").unwrap();
+        assert_eq!(data0.as_object().unwrap().len(), 5);
+        assert_eq!(data0.get("token_in").unwrap(), "wrap.near");
+        assert_eq!(data0.get("token_out").unwrap(), "test.near");
+        assert_eq!(data0.get("amount_in").unwrap(), 100);
+        assert_eq!(data0.get("amount_out").unwrap(), 200);
+        assert_eq!(data0.get("test").unwrap(), "tst");
+    }
+    {
+        let log1: serde_json::Value =
+            serde_json::from_str(&logs[1].strip_prefix("EVENT_JSON:").unwrap()).unwrap();
+        assert_eq!(log1.as_object().unwrap().len(), 4);
+        assert_eq!(log1.get("standard").unwrap(), "test_standard");
+        assert_eq!(log1.get("version").unwrap(), "2.0.0");
+        assert_eq!(log1.get("event").unwrap(), "string_event");
+        assert_eq!(log1.get("data").unwrap(), "string");
+    }
+    {
+        assert_eq!(log2.as_object().unwrap().len(), 3);
+        assert_eq!(log2.get("standard").unwrap(), "test_standard");
+        assert_eq!(log2.get("version").unwrap(), "3.0.0");
+        assert_eq!(log2.get("event").unwrap(), "empty_event");
+        assert!(log2.get("data").is_none());
+    }
+    {
+        let log3: serde_json::Value =
+            serde_json::from_str(&logs[3].strip_prefix("EVENT_JSON:").unwrap()).unwrap();
+        assert_eq!(log3.as_object().unwrap().len(), 4);
+        assert_eq!(log3.get("standard").unwrap(), "test_standard");
+        assert_eq!(log3.get("version").unwrap(), "4.0.0");
+        assert_eq!(log3.get("event").unwrap(), "lifetime_test_a");
+        assert_eq!(log3.get("data").unwrap(), "lifetime");
+    }
+    {
+        let log4: serde_json::Value =
+            serde_json::from_str(&logs[4].strip_prefix("EVENT_JSON:").unwrap()).unwrap();
+        assert_eq!(log4.as_object().unwrap().len(), 4);
+        assert_eq!(log4.get("standard").unwrap(), "test_standard");
+        assert_eq!(log4.get("version").unwrap(), "5.0.0");
+        assert_eq!(log4.get("event").unwrap(), "lifetime_test_b");
+        assert_eq!(log4.get("data").unwrap(), "lifetime_b");
+    }
+    {
+        let log5: serde_json::Value =
+            serde_json::from_str(&logs[5].strip_prefix("EVENT_JSON:").unwrap()).unwrap();
+        assert_eq!(log5.as_object().unwrap().len(), 3);
+        assert_eq!(log5.get("standard").unwrap(), "another_standard");
+        assert_eq!(log5.get("version").unwrap(), "1.0.0");
+        assert_eq!(log5.get("event").unwrap(), "test");
+        assert!(log5.get("data").is_none());
+    }
 }
